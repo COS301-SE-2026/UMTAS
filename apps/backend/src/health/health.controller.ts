22a58df95c5c2fedbbb5c2712b8f4cfe0d5ea22c
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2026-05-16 14:56:37 +0200

    feat(infra): enhance health check and mailer service

--- apps/backend/src/health/health.controller.ts
+++ apps/backend/src/health/health.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
-import { Roles } from '../auth/roles.guard';
 
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
-  @Post('hello')
-  @Roles('student')
-  async hello() {
-    const entity = await this.healthService.writeHello();
-
-    return {
-      success: true,
-      message: entity.message,
-      id: entity.id,
-      createdAt: entity.createdAt,
-    };
+  @Get()
+  async check() {
+    return this.healthService.check();
   }
 }
