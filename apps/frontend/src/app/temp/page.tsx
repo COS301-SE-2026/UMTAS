8bee78706497e3529a94d2aea63424ac86d3d5c5
jcoet-gh <johancoet9@gmail.com>
2026-05-18 16:45:33 +0200

    feat: added event dropdown components

--- apps/frontend/src/app/temp/page.tsx
+++ apps/frontend/src/app/temp/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 import React, { useState } from "react";
 import { ColourPicker } from "@/components/organisms/builder/colourPicker";
+import {
+  EventTypeDropdown,
+  EventType,
+} from "@/components/organisms/builder/eventDropdown";
 
 export default function temp() {
   const [selectedColour, setSelectedColour] = useState("#4A90F8");
+  const [selectedEvent, setSelectedEvent] = useState<EventType>("lecture");
   return (
-    <div className="p-4">
-      <ColourPicker
-        value={selectedColour}
-        onChange={(newColour) => setSelectedColour(newColour)}
+    <div>
+      <div className="p-4">
+        <ColourPicker
+          value={selectedColour}
+          onChange={(newColour) => setSelectedColour(newColour)}
+        />
+        <p className="mt-4">colour code: {selectedColour}</p>
+      </div>
+
+      <EventTypeDropdown
+        value={selectedEvent}
+        onChange={(newEvent) => setSelectedEvent(newEvent)}
       />
-      <p className="mt-4">colour code: {selectedColour}</p>
     </div>
   );
 }
