8df14f814cc7ac9fdae353286e63ed6e76fd0920
Wilmar-Smit <u24584216@tuks.co.za>
2026-05-18 10:41:54 +0200

    feat(modules): added schema for modules

---
+++ apps/backend/src/entities/Modules/modules.schema.ts
@@ -0,0 +1,9 @@
+import { jsonb, pgTable, serial, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const modules = pgTable('modules', {
+  moduleID: serial('moduleID').primaryKey(),
+  moduleCode: varchar('moduleCode', { length: 6 }).notNull(),
+  moduleName: varchar('moduleName', { length: 256 }).notNull(),
+  styling: jsonb('styling'),
+  userID: uuid('userID').notNull(),
+});
