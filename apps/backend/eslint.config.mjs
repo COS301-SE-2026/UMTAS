0429e0592db1c2d16ab9316288ec66c62f9fe147
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-05-19 10:05:32 +0200

    refactor(config): added gitkeep

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -28,6 +28,7 @@ export default [
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
