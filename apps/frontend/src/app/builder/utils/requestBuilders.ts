cfa7d51854a996d34da96198cea90430d8fe89c9
Wilmar-Smit <u24584216@tuks.co.za>
2026-05-19 21:19:08 +0200

    feat(api integration): trying to get the builder to be more universal

--- apps/frontend/src/app/builder/utils/requestBuilders.ts
+++ apps/frontend/src/app/builder/utils/requestBuilders.ts
@@ -4,8 +4,12 @@ type createModuleReq =
   paths["/modules"]["post"]["requestBody"]["content"]["application/json"];
 type createModuleRes =
   paths["/modules"]["post"]["responses"]["201"]["content"]["application/json"]["module"];
-
-type getAllModules = paths["/modules"]["get"]["requestBody"];
+type getAllModulesRes =
+  paths["/modules"]["get"]["responses"]["200"]["content"]["application/json"]["modules"];
+type getModuleByIdReq =
+  paths["/modules/{moduleId}"]["get"]["parameters"]["path"];
+type getModuleByIdRes =
+  paths["/modules/{moduleId}"]["get"]["responses"]["200"]["content"]["application/json"]["module"];
 
 export class createModulesBuilder extends RequestBuilder<
   createModuleReq,
@@ -14,5 +18,25 @@ export class createModulesBuilder extends RequestBuilder<
   constructor() {
     super();
     this.setUrl("/modules").setMethod(RequestMethod.POST);
+  }
+}
+
+export class getAllModulesBuilder extends RequestBuilder<
+  undefined,
+  getAllModulesRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/modules").setMethod(RequestMethod.GET);
+  }
+}
+
+export class getModulesByIdBuilder extends RequestBuilder<
+  getModuleByIdReq,
+  getModuleByIdRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/modules/{moduleId}").setMethod(RequestMethod.GET);
   }
 }
--- apps/frontend/utilities/request.ts
+++ apps/frontend/utilities/request.ts
@@ -5,14 +5,14 @@ enum RequestMethod {
   DELETE = "DELETE",
 }
 
-export class RequestBuilder<RequestType, ResponseType> {
+export class RequestBuilder<RequestType, ResponseType, PathType = undefined> {
   private url: string = "";
   private method: RequestMethod = RequestMethod.GET;
   private headers: Record<string, string> = {
     "Content-Type": "application/json",
   };
 
-  public setUrl(url: string): this {
+  protected setUrl(url: string): this {
     const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
     const cleanBase = baseUrl.replace(/\/$/, "");
     const cleanPath = url.replace(/^\//, "");
@@ -20,35 +20,51 @@ export class RequestBuilder<RequestType, ResponseType> {
     return this;
   }
 
-  public setMethod(method: RequestMethod): this {
+  protected setMethod(method: RequestMethod): this {
     this.method = method;
     return this;
   }
 
-  public setHeaders(headers: Record<string, string>): this {
+  protected setHeaders(headers: Record<string, string>): this {
     this.headers = { ...this.headers, ...headers };
     return this;
   }
 
-  public setBearerToken(token: string): this {
+  protected setBearerToken(token: string): this {
     this.headers["Authorization"] = `Bearer ${token}`;
     return this;
   }
 
   public async send(body?: RequestType): Promise<ResponseType> {
-    const methodsRequiringBody: string[] = [
+    const methodsRequiringBody: RequestMethod[] = [
       RequestMethod.POST,
       RequestMethod.PUT,
     ];
+
     if (methodsRequiringBody.includes(this.method) && body === undefined) {
       throw new Error(`Request body required for ${this.method} requests`);
     }
 
-    const response = await fetch(this.url, {
+    let finalUrl = this.url;
+
+    if (this.method === RequestMethod.GET && body !== undefined) {
+      const params = new URLSearchParams();
+      Object.entries(body as Record<string, string | number | boolean>).forEach(
+        ([key, value]) => {
+          params.append(key, String(value));
+        },
+      );
+      finalUrl += `?${params.toString()}`;
+    }
+
+    const response = await fetch(finalUrl, {
       method: this.method,
       headers: this.headers,
       credentials: "include",
-      body: body !== undefined ? JSON.stringify(body) : undefined,
+      body:
+        this.method !== RequestMethod.GET && body !== undefined
+          ? JSON.stringify(body)
+          : undefined,
     });
 
     if (!response.ok) {
@@ -59,6 +75,16 @@ export class RequestBuilder<RequestType, ResponseType> {
         errorBody = "(could not read response body)";
       }
       throw new Error(`HTTP ${response.status}: ${errorBody}`);
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (
+      response.status === 204 ||
+      response.headers.get("content-length") === "0" ||
+      !contentType ||
+      !contentType.includes("application/json")
+    ) {
+      return {} as ResponseType;
     }
 
     return (await response.json()) as ResponseType;
