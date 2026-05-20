ca2f02c28c59ae9289cb281066c8b37332d57087
sdcreek240 <aidandwsn@gmail.com>
2026-05-20 13:14:38 +0200

    feat(integration): smoke tests just to see CI pipe working for integration

---
+++ apps/backend/src/Events/integration/smoke.int.ts
@@ -0,0 +1,5 @@
+describe('Event - Integration Smoke Test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ apps/backend/src/Module/integration/smoke.int.ts
@@ -0,0 +1,5 @@
+describe('Module - Integration Smoke Test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
