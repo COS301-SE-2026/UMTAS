3bda637ca4f3642afa70a37d6a8974e8e0335075
Wilmar-Smit <u24584216@tuks.co.za>
2026-05-20 16:01:04 +0200

    feat(api-integration): added endpoint builders

--- apps/frontend/src/app/builder/utils/events/eventRequestBuilder.ts
+++ apps/frontend/src/app/builder/utils/events/eventRequestBuilder.ts
@@ -5,4 +5,45 @@ import {
 
 import { paths } from "@/lib/api";
 
-type createEvent = paths;
+type createEventBody =
+  paths["/events"]["post"]["requestBody"]["content"]["application/json"];
+type createEventRes =
+  paths["/events"]["post"]["responses"]["201"]["content"]["application/json"];
+
+type getAllEventsRes =
+  paths["/events"]["get"]["responses"]["200"]["content"]["application/json"];
+
+type getEventByIDPath = paths["/events/{id}"]["get"]["parameters"]["path"];
+
+export class createEventsBuilder extends RequestBuilder<
+  undefined,
+  createEventBody,
+  createEventRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/events").setMethod(RequestMethod.POST);
+  }
+}
+
+export class getAllEventsBuilder extends RequestBuilder<
+  undefined,
+  undefined,
+  getAllEventsRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/events").setMethod(RequestMethod.GET);
+  }
+}
+
+export class getEventByIDBuilder extends RequestBuilder<
+  getEventByIDPath,
+  undefined,
+  undefined
+> {
+  constructor() {
+    super();
+    this.setUrl("/events/{id}").setMethod(RequestMethod.GET);
+  }
+}
