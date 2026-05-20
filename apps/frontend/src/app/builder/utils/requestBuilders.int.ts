c76018c60857a4508ac45d31c2a774b05774038d
Wilmar-Smit <u24584216@tuks.co.za>
2026-05-20 11:29:06 +0200

    feat(api integration): added added create integration checks first if there is a module of that code

--- apps/frontend/src/app/builder/utils/requestBuilders.int.ts
+++ apps/frontend/src/app/builder/utils/requestBuilders.int.ts
@@ -1,7 +1,12 @@
 /**
  * @jest-environment node
  */
-import { getAllModulesBuilder } from "./requestBuilders";
+import {
+  getAllModulesBuilder,
+  createModulesBuilder,
+  deleteModulesById,
+  createModuleReq,
+} from "./requestBuilders";
 
 const apiUrl = process.env.API_URL || "http://localhost:3000";
 const testEmail = process.env.SEED_SYSTEM_ADMIN_EMAIL;
@@ -44,16 +49,42 @@ describe("Request Builders Integration Tests", () => {
   describe("getAllModulesBuilder", () => {
     it("should return modules successfully", async () => {
       const builder = new getAllModulesBuilder();
+
       if (sessionCookie) {
         builder.setHeaders({ Cookie: sessionCookie });
       }
-      const result = await builder.send({});
 
-      console.log(
-        "Builder Response (Success):",
-        JSON.stringify(result, null, 2),
-      );
+      const result = await builder.send({});
+      console.log("getAllModules Response:", JSON.stringify(result, null, 2));
+
       expect(Array.isArray(result.modules)).toBe(true);
+    });
+  });
+
+  describe("createModulesBuilder", () => {
+    it("should create a module successfully", async () => {
+      const builder = new createModulesBuilder();
+      const deleteBuilder = new deleteModulesById();
+
+      if (sessionCookie) {
+        deleteBuilder.setHeaders({ Cookie: sessionCookie });
+        builder.setHeaders({ Cookie: sessionCookie });
+      }
+      deleteBuilder.send({ paths: { moduleId: 2 } });
+      const mockModule: createModuleReq = {
+        code: `COS301`,
+        name: "Integration Test Module",
+        description: "Created by integration test",
+        userId: "550e8400-e29b-41d4-a716-446655440000", // type expects it backend doesnt care
+        styling: "#FF0000",
+      };
+
+      const result = await builder.send({ body: mockModule });
+      console.log("createModules Response:", JSON.stringify(result, null, 2));
+
+      expect(result.module).toBeDefined();
+      expect(result.module.moduleCode).toBe(mockModule.code);
+      expect(result.module.moduleName).toBe(mockModule.name);
     });
   });
 });
