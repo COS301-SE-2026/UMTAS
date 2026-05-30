0fb448c315c63656aab62bc8a4c5f145b2c2eab1
sdcreek240 <aidandwsn@gmail.com>
2026-05-30 13:39:36 +0200

    feat(testing): timetable testing coverage now 100%

--- apps/backend/src/Timetable/timetable.service.spec.ts
+++ apps/backend/src/Timetable/timetable.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { TimetableService } from './timetable.service';
 import { DatabaseService } from '../db/database.service';
 
@@ -61,7 +65,94 @@ describe('TimetableService', () => {
       expect(result.timetable).toEqual(newTimetable);
       expect(result.eventIds).toBeUndefined();
     });
-  });
+
+    it('should throw InternalServerException for timetable not created', async () => {
+      dbService.db.transaction.mockImplementation(
+        (cb: (tx: any) => Promise<any>) => {
+          const mockTx = {
+            insert: jest.fn().mockReturnValue({
+              values: jest.fn().mockReturnValue({
+                returning: jest.fn().mockResolvedValue([]),
+              }),
+            }),
+          };
+
+          return cb(mockTx);
+        },
+      );
+
+      await expect(
+        service.createTimetable('user-1', { timetableName: 'testy' }),
+      ).rejects.toThrow(InternalServerErrorException);
+    });
+
+    it('should throw if eventIds dont exist', async () => {
+      dbService.db.transaction.mockImplementation(
+        (cb: (tx: any) => Promise<any>) => {
+          const mockTx = {
+            insert: jest.fn().mockReturnValue({
+              values: jest.fn().mockReturnValue({
+                returning: jest
+                  .fn()
+                  .mockResolvedValue([{ timetableID: 1, userID: 'user-1' }]),
+              }),
+            }), //insert
+            select: jest.fn().mockReturnValue({
+              from: jest.fn().mockReturnValue({
+                where: jest.fn().mockResolvedValue([{ eventID: 1 }]),
+              }),
+            }), //select
+          };
+
+          return cb(mockTx);
+        },
+      );
+
+      await expect(
+        service.createTimetable('user-1', {
+          timetableName: 'Test',
+          eventIds: [1, 2, 3],
+        }),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should create timetable if all events are valid', async () => {
+      dbService.db.transaction.mockImplementation(
+        (cb: (tx: any) => Promise<any>) => {
+          const mockTx = {
+            insert: jest
+              .fn()
+              .mockReturnValueOnce({
+                values: jest.fn().mockReturnValue({
+                  returning: jest
+                    .fn()
+                    .mockResolvedValue([{ timetableID: 1, userID: 'user-1' }]),
+                }),
+              })
+              .mockReturnValueOnce({
+                values: jest.fn().mockResolvedValue([]),
+              }), //insert
+
+            select: jest.fn().mockReturnValue({
+              from: jest.fn().mockReturnValue({
+                where: jest
+                  .fn()
+                  .mockResolvedValue([{ eventID: 1 }, { eventID: 2 }]),
+              }),
+            }), //select
+          };
+
+          return cb(mockTx);
+        },
+      );
+
+      const timetable = await service.createTimetable('user-1', {
+        eventIds: [1, 2],
+      });
+
+      expect(timetable.eventIds).toEqual([1, 2]);
+    });
+  }); //create - done
 
   describe('getAllTimetables', () => {
     it('should return timetables with their linked eventIds grouped correctly', async () => {
@@ -85,7 +176,7 @@ describe('TimetableService', () => {
       expect(result.timetables[0].eventIds).toEqual([10, 11]);
       expect(result.timetables[1].eventIds).toBeUndefined();
     });
-  });
+  }); //get all done
 
   describe('getTimetableById', () => {
     it('should throw NotFoundException when timetable does not exist', async () => {
@@ -101,6 +192,29 @@ describe('TimetableService', () => {
         NotFoundException,
       );
     });
+
+    it('should return timetable with events with timetable id', async () => {
+      const timetable = {
+        timetableID: 1,
+        userID: 'user-1',
+        timetableName: 'Test',
+      };
+
+      dbService.db.select.mockReturnValue({
+        from: jest.fn().mockReturnValue({
+          leftJoin: jest.fn().mockReturnValue({
+            where: jest.fn().mockResolvedValue([
+              { timetable, eventID: 1 },
+              { timetable, eventID: 2 },
+            ]),
+          }),
+        }),
+      });
+
+      const result = await service.getTimetableById('user-1', 1);
+      expect(result.timetable).toEqual(timetable);
+      expect(result.eventIds).toEqual([1, 2]);
+    });
   });
 
   describe('updateTimetable', () => {
@@ -108,6 +222,131 @@ describe('TimetableService', () => {
       await expect(service.updateTimetable('user-1', 1, {})).rejects.toThrow(
         BadRequestException,
       );
+    });
+
+    it('should throw NotFoundException if there is o timetable to update', async () => {
+      dbService.db.transaction.mockImplementation(
+        (cb: (tx: any) => Promise<any>) => {
+          const mockTx = {
+            select: jest.fn().mockReturnValue({
+              from: jest.fn().mockReturnValue({
+                where: jest.fn().mockReturnValue({
+                  limit: jest.fn().mockResolvedValue([]),
+                }),
+              }),
+            }),
+          };
+
+          return cb(mockTx);
+        },
+      );
+
+      await expect(
+        service.updateTimetable('user-1', 1, { timetableName: 'hallo' }),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should update name, and remove event, and add event', async () => {
+      const timetable = makeTimetable();
+      const newTimetable = makeTimetable({ timetableName: 'newName' });
+
+      dbService.db.transaction.mockImplementation(
+        (cb: (tx: any) => Promise<any>) => {
+          let selectCallCount = 0;
+
+          const mockTx = {
+            select: jest.fn().mockImplementation(() => {
+              selectCallCount++;
+              if (selectCallCount === 1) {
+                return {
+                  from: jest.fn().mockReturnValue({
+                    where: jest.fn().mockReturnValue({
+                      limit: jest.fn().mockResolvedValue([timetable]),
+                    }),
+                  }),
+                };
+              }
+
+              return {
+                from: jest.fn().mockReturnValue({
+                  where: jest.fn().mockResolvedValue([{ eventID: 3 }]),
+                }),
+              };
+            }), //select
+
+            update: jest.fn().mockReturnValue({
+              set: jest.fn().mockReturnValue({
+                where: jest.fn().mockReturnValue({
+                  returning: jest.fn().mockResolvedValue([newTimetable]),
+                }),
+              }),
+            }), //udpate
+
+            insert: jest.fn().mockReturnValue({
+              values: jest.fn().mockReturnValue({
+                onConflictDoNothing: jest.fn().mockResolvedValue([]),
+              }),
+            }), //insert
+
+            delete: jest.fn().mockReturnValue({
+              where: jest.fn().mockResolvedValue([]),
+            }),
+          };
+
+          return cb(mockTx);
+        },
+      );
+
+      dbService.db.select.mockReturnValue({
+        from: jest.fn().mockReturnValue({
+          leftJoin: jest.fn().mockReturnValue({
+            where: jest
+              .fn()
+              .mockResolvedValue([{ timetable: newTimetable, eventID: 3 }]),
+          }),
+        }),
+      });
+
+      const result = await service.updateTimetable('user-1', 1, {
+        timetableName: 'newName',
+        addEventIds: [3],
+        removeEventIds: [1],
+      });
+
+      expect(result.timetable.timetableName).toBe('newName');
+      expect(result.eventIds).toEqual([3]);
+    });
+
+    it('should throw InternalServerException when timetable not updated', async () => {
+      const timetable = makeTimetable();
+
+      dbService.db.transaction.mockImplementation(
+        (cb: (tx: any) => Promise<any>) => {
+          const mockTx = {
+            select: jest.fn().mockReturnValue({
+              from: jest.fn().mockReturnValue({
+                where: jest.fn().mockReturnValue({
+                  limit: jest.fn().mockResolvedValue([timetable]),
+                }),
+              }),
+            }),
+
+            update: jest.fn().mockReturnValue({
+              set: jest.fn().mockReturnValue({
+                where: jest.fn().mockReturnValue({
+                  returning: jest.fn().mockResolvedValue([]),
+                }),
+              }),
+            }),
+          };
+
+          return cb(mockTx);
+        },
+      );
+
+      await expect(
+        service.updateTimetable('user-1', 1, { timetableName: 'newName' }),
+      ).rejects.toThrow(InternalServerErrorException);
     });
   });
 
@@ -123,6 +362,49 @@ describe('TimetableService', () => {
 
       await expect(service.deleteTimetable('user-1', 999)).rejects.toThrow(
         NotFoundException,
+      );
+    });
+
+    it('should delete timetable', async () => {
+      const timetable = makeTimetable();
+
+      dbService.db.select.mockReturnValue({
+        from: jest.fn().mockReturnValue({
+          where: jest.fn().mockReturnValue({
+            limit: jest.fn().mockResolvedValue([timetable]),
+          }),
+        }),
+      });
+
+      dbService.db.delete.mockReturnValue({
+        where: jest.fn().mockReturnValue({
+          returning: jest.fn().mockResolvedValue([timetable]),
+        }),
+      });
+
+      const result = await service.deleteTimetable('user-1', 1);
+      expect(result).toEqual({ success: true });
+    });
+
+    it('should throw InternalServerException for timetable not deleted', async () => {
+      const timetable = makeTimetable();
+
+      dbService.db.select.mockReturnValue({
+        from: jest.fn().mockReturnValue({
+          where: jest.fn().mockReturnValue({
+            limit: jest.fn().mockResolvedValue([timetable]),
+          }),
+        }),
+      });
+
+      dbService.db.delete.mockReturnValue({
+        where: jest.fn().mockReturnValue({
+          returning: jest.fn().mockResolvedValue([]),
+        }),
+      });
+
+      await expect(service.deleteTimetable('user-1', 1)).rejects.toThrow(
+        InternalServerErrorException,
       );
     });
   });
