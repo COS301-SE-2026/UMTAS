5183668dfbad65e099eeb9a5c1a2a36f5ad7d694
Wilmar-Smit <u24584216@tuks.co.za>
2026-06-23 16:56:07 +0200

    feat(added userTimetable): user timetables

--- apps/backend/src/entities/timetables/timetables.schema.ts
+++ apps/backend/src/entities/timetables/timetables.schema.ts
@@ -6,12 +6,15 @@ import {
   integer,
   primaryKey,
 } from 'drizzle-orm/pg-core';
-import { Event } from '../Events';
+import { UniversityEvent } from '../Events';
+import { usersTable } from '../auth';
 
 export const Timetable = pgTable('Timetable', {
   timetableID: serial('timetableID').primaryKey(),
   timetableName: varchar('timetableName', { length: 32 }),
-  userID: uuid('userID').notNull(),
+  userID: uuid('userID')
+    .references(() => usersTable.id, { onDelete: 'cascade' })
+    .notNull(),
 });
 
 export const EventsToTimetables = pgTable(
@@ -19,10 +22,22 @@ export const EventsToTimetables = pgTable(
   {
     eventID: integer('eventID')
       .notNull()
-      .references(() => Event.eventID, { onDelete: 'cascade' }),
+      .references(() => UniversityEvent.UniversityEventID, {
+        onDelete: 'cascade',
+      }), // changed to link specifically to a Uni child event it should not break....
     timetableID: integer('timetableID')
       .notNull()
       .references(() => Timetable.timetableID, { onDelete: 'cascade' }),
   },
   (table) => [primaryKey({ columns: [table.eventID, table.timetableID] })],
 );
+
+export const UserTimetable = pgTable('UserTimetable', {
+  UserTimetableID: uuid('UserTimetableID').defaultRandom().primaryKey(),
+  UserID: uuid('UserID')
+    .references(() => usersTable.id, { onDelete: 'cascade' })
+    .notNull(),
+  TimetableID: integer('TimetableID')
+    .references(() => Timetable.timetableID)
+    .notNull(),
+});
