45a292b615592ecef8f2c72311ed31b564464c4f
Wilmar-Smit <u24584216@tuks.co.za>
2026-06-24 22:31:48 +0200

    feat(added TSQ): watched a video and added

---
+++ apps/frontend/src/components/tanstack/tanstackProvider.tsx
@@ -0,0 +1,14 @@
+"use-client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { getQueryClient } from "./getQueryClient";
+
+export interface TanstackProviderProps {
+  children: ReactNode;
+}
+
+export default function TanstackProvider({ children }: TanstackProviderProps) {
+  const client = getQueryClient();
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
