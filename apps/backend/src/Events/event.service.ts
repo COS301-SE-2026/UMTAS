7db7fd2830a6b34b3f1f5dcfacffd064a72043f0
Aidan Dawson <143823337+sdcreek240@users.noreply.github.com>
2026-06-29 14:23:47 +0200

    Fix ownership check logic for events
    Signed-off-by: Aidan Dawson <143823337+sdcreek240@users.noreply.github.com>

--- apps/backend/src/Events/event.service.ts
+++ apps/backend/src/Events/event.service.ts
@@ -94,7 +94,7 @@ export class EventService {
     //Ownership check
     //Student can update event if its created from a module that is STUDENT_OWNED
     //If module isn't STUDENT_OWNED user needs to be admin/lecturer
-    if (role==='student' && await this.ownershipCheck(userId, eventId)) 
+    if (role==='student' && !await this.ownershipCheck(userId, eventId)) 
       throw new ForbiddenException(`User[${userId}][${role}] cannot update event they don't own`);
     //If not a student, no ownership check necessary
 
@@ -151,7 +151,7 @@ export class EventService {
    Promise<DeleteResponseDto> {
  
     //Ownership check
-    if (role==='student' && await this.ownershipCheck(userId, eventId)) 
+    if (role==='student' && !await this.ownershipCheck(userId, eventId)) 
       throw new ForbiddenException(`User[${userId}][${role}] cannot update event they don't own`);
 
     //fetch event
