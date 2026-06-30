5ab1098f3a754ab48eb66e4a0ec8ba0cf1bd559e
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-06-30 12:48:30 +0200

    feat(frontend): added select role molecule

---
+++ apps/frontend/src/components/molecules/choose-institute/SelectRoleField.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Label } from "@/components/atoms/baseShadcn/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/atoms/baseShadcn/select";
+
+const roles = ["Student", "Lecturer", "University_admin"]; //will change with dto
+
+interface SelectRoleFieldProps {
+  value: string; //todo: adjust when willie tells me to
+  onChange: (value: string) => void;
+}
+
+export function SelectRoleField({ value, onChange }: SelectRoleFieldProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <Label htmlFor="role-select">Select Role</Label>
+      <Select value={value} onValueChange={onChange}>
+        <SelectTrigger id="role-select">
+          <SelectValue placeholder="Select a role" />
+        </SelectTrigger>
+        <SelectContent>
+          {roles.map((role) => (
+            <SelectItem key={role} value={role}>
+              {role}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
