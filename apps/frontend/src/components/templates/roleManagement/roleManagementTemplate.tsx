cd38da9a88e4b7442fd5d4c25c3db51781c144eb
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-06-30 23:14:36 +0200

    feat(frontend): added tabs for the role management pages

--- apps/frontend/src/components/templates/roleManagement/roleManagementTemplate.tsx
+++ apps/frontend/src/components/templates/roleManagement/roleManagementTemplate.tsx
@@ -1,9 +1,37 @@
 import UserDirectory from "@/components/organisms/roleManagement/UserDirectory";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/atoms/baseShadcn/tabs";
 
 export default async function RoleManagementTemplate() {
   return (
     <div className="container mx-auto py-10">
-      <UserDirectory />
+      <h1 className="text-lg font-semibold text-[var(--text-primary)]">
+        Role Management
+      </h1>
+      <p className="text-sm text-[var(--text-secondary)]">
+        Manage user roles and approve pending applications.
+      </p>
+
+      <Tabs defaultValue="User Directory" className="w-full">
+        <TabsList>
+          <TabsTrigger value="User Directory">User Directory</TabsTrigger>
+          <TabsTrigger value="Pending Applications">
+            Pending Applications
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="User Directory">
+          {" "}
+          <UserDirectory />
+        </TabsContent>
+        <TabsContent value="Pending Applications">
+          {" "}
+          <UserDirectory />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
