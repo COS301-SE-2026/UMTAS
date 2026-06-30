299e48369c545a76a51d1ee623eb6540e1157e58
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-06-30 13:07:27 +0200

    feat(frontend): added status molecule for role approval

---
+++ apps/frontend/src/components/molecules/choose-institute/ApprovalStatus.tsx
@@ -0,0 +1,47 @@
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/atoms/baseShadcn/alert";
+
+interface ApprovalStatusProps {
+  status: "approved" | "pending" | "rejected";
+  universityName: string;
+}
+
+export function ApprovalStatus({
+  status,
+  universityName,
+}: ApprovalStatusProps) {
+  if (status === "approved") {
+    return (
+      <Alert variant="success">
+        <AlertTitle>Role approved</AlertTitle>
+        <AlertDescription>
+          Your role has been approved for {universityName}. You can now access
+          the university&apos;s resources.
+        </AlertDescription>
+      </Alert>
+    );
+  } else if (status === "rejected") {
+    return (
+      <Alert variant="destructive">
+        <AlertTitle>Role rejected</AlertTitle>
+        <AlertDescription>
+          Your role has been rejected for {universityName}. Please contact the
+          university for more information.
+        </AlertDescription>
+      </Alert>
+    );
+  } else if (status === "pending") {
+    return (
+      <Alert variant="default">
+        <AlertTitle>Role not yet approved</AlertTitle>
+        <AlertDescription>
+          Your role has not yet been approved for {universityName}. Please wait
+          for the approval process to complete.
+        </AlertDescription>
+      </Alert>
+    );
+  }
+}
