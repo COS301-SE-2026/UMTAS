55df5964e7450f50113e12b691a552dfadbe645d
Wilmar-Smit <u24584216@tuks.co.za>
2026-06-30 16:16:13 +0200

    feat(ga): added stuff

--- apps/preference-solver/src/data/API/API-data.cpp
+++ apps/preference-solver/src/data/API/API-data.cpp
@@ -1,6 +1,15 @@
 #include "API-data.h"
+#include <stdexcept>
 API_DATA::API_DATA(string jsonStr) {
-  json reqData = json::parse(jsonStr);
-  json modules = reqData[ModuleGA::GROUPING_KEY];
-  this->modules = ModuleGA::innitModules(modules);
+  try {
+    json reqData = json::parse(jsonStr);
+    json modules = reqData[ModuleGA::GROUPING_KEY];
+    this->modules = ModuleGA::innitModules(modules);
+  } catch (const json::parse_error &e) {
+      // this is for errors casued by library misuse
+    throw std::runtime_error(string("Json error: ") + e.what());
+  } catch (const std::runtime_error &e) {
+      // this is for our errors
+    throw std::runtime_error(string("Could not create API_DATA: ") + e.what());
+  }
 }
--- apps/preference-solver/src/data/Modules/module.cpp
+++ apps/preference-solver/src/data/Modules/module.cpp
@@ -2,11 +2,26 @@
 #include <stdexcept>
 #include <vector>
 
- const string ModuleGA::GROUPING_KEY = "modules";
- const string ModuleGA::MODULE_CODE_KEY = "moduleCode";
- const string ModuleGA::OCCURENCE_KEY ="moduleOccurence";
+const string ModuleGA::GROUPING_KEY = "modules";
+const string ModuleGA::MODULE_CODE_KEY = "moduleCode";
+const string ModuleGA::OCCURENCE_KEY = "moduleOccurence";
 
-ModuleGA::ModuleGA(json module) {}
+ModuleGA::ModuleGA(json module) {
+  if (module.contains(MODULE_CODE_KEY) && module[MODULE_CODE_KEY].is_string()) {
+    this->moduleCode = module[MODULE_CODE_KEY];
+  } else {
+    throw std::runtime_error(MODULE_CODE_KEY +
+                             "is not defined or is not a string");
+  }
+
+  if (module.contains(OCCURENCE_KEY) &&
+      module[OCCURENCE_KEY].is_number_integer()) {
+    this->moduleCode = module[OCCURENCE_KEY];
+  } else {
+    throw std::runtime_error(OCCURENCE_KEY +
+                             "is not defined or is not an Integer");
+  }
+}
 
 std::vector<ModuleGA> ModuleGA::innitModules(json &modulesArr) {
   std::vector<ModuleGA> retModules;
