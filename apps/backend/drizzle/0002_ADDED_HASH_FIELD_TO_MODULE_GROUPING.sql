080964dc0c81e467c1b232b72edfbdd520e07b62
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2026-07-03 15:22:02 +0200

    fix: migrations

--- apps/backend/drizzle/0002_ADDED_HASH_FIELD_TO_MODULE_GROUPING.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "Course" DROP CONSTRAINT "Course_GroupID_ModuleGrouping_GroupID_fk";
---> statement-breakpoint
-ALTER TABLE "Course" ALTER COLUMN "GroupID" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "ModuleGrouping" ADD COLUMN "hash" varchar(64) NOT NULL;--> statement-breakpoint
-ALTER TABLE "Course" ADD CONSTRAINT "Course_GroupID_ModuleGrouping_GroupID_fk" FOREIGN KEY ("GroupID") REFERENCES "public"."ModuleGrouping"("GroupID") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "ModuleGrouping" DROP COLUMN "GroupName";--> statement-breakpoint
-ALTER TABLE "ModuleGrouping" ADD CONSTRAINT "ModuleGrouping_hash_unique" UNIQUE("hash");
