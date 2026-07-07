5f7db099e426b48c44dbc42431146ac83871a935
Wilmar-Smit <u24584216@tuks.co.za>
2026-07-07 13:07:32 +0200

    feat(Roles): added fetch request for queries

--- apps/frontend/src/app/course-management/queries/courses/courseBuilder.ts
+++ apps/frontend/src/app/course-management/queries/courses/courseBuilder.ts
@@ -20,3 +20,39 @@ export class createCoursesBuilder extends RequestBuilder<
     this.setUrl("/Courses").setMethod(RequestMethod.POST);
   }
 }
+export type fetchAllCourses =
+  paths["/Courses/university/{universityId}"]["get"];
+export type fetchAllCoursesQueries = fetchAllCourses["parameters"]["query"];
+export type fetchAllCourseRes =
+  fetchAllCourses["responses"]["200"]["content"]["application/json"];
+
+export async function fetchAllCoursesRequest(queries: fetchAllCoursesQueries) {
+  const baseUrl =
+    (typeof window === "undefined"
+      ? process.env.API_URL
+      : process.env.NEXT_PUBLIC_API_URL) || "http://localhost:3000";
+
+  const searchParams = new URLSearchParams();
+  if (queries?.UniversityID) {
+    searchParams.append("UniversityID", queries.UniversityID);
+  }
+  if (queries?.Degree) {
+    searchParams.append("Degree", queries.Degree);
+  }
+  const queryStr = searchParams.toString();
+  const URL = queryStr ? `${baseUrl}?${queryStr}` : baseUrl;
+
+  const response = await fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch courses");
+  } else {
+    const data = await response.json();
+    return data as fetchAllCourseRes;
+  }
+}
