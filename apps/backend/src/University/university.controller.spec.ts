fc7629849f0b186c3c23e157443f0457aa20b33d
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-07-08 11:25:53 +0200

    test(backend): added university skeletons

---
+++ apps/backend/src/University/university.controller.spec.ts
@@ -0,0 +1,64 @@
+import { UniversityController } from './university.controller';
+import { UniversityService } from './university.service';
+
+import { Test } from '@nestjs/testing';
+
+//Constants
+import { uniId } from '../Testing/constants.spec';
+
+//Mock services
+import { createMockUniversityService } from '../Testing/Mocks/services';
+
+//Factories
+
+import { CreateUniversityDto } from './dto/university.dto';
+
+//DTo's
+import { UniversitySingleResponseDto } from './dto/university.dto';
+
+describe('UniversityController', () => {
+  let controller: UniversityController;
+
+  //mock services
+  const { mockUniversityService, reset: resetUni } =
+    createMockUniversityService();
+
+  // const mockSession = createMockSession(userId, 'student');
+  //before
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [UniversityController],
+      providers: [
+        { provide: UniversityService, useValue: mockUniversityService },
+      ],
+    }).compile();
+
+    controller = module.get<UniversityController>(UniversityController);
+  }); //END_BeforeEach
+
+  //after
+  afterEach(() => {
+    resetUni();
+    jest.clearAllMocks();
+  }); //END_afterEach
+
+  //TESTS
+
+  describe('TEST_create', () => {
+    it('should create a university', async () => {
+      const createDto: CreateUniversityDto = {
+        UniversityName: 'Test University',
+      };
+      const expectedResponse: UniversitySingleResponseDto = {
+        UniversityName: createDto.UniversityName,
+        UniversityID: uniId,
+      };
+
+      mockUniversityService.create!.mockResolvedValue(expectedResponse);
+
+      const result = await controller.create(createDto);
+      expect(result).toEqual(expectedResponse);
+      expect(mockUniversityService.create).toHaveBeenCalledWith(createDto);
+    });
+  });
+});
