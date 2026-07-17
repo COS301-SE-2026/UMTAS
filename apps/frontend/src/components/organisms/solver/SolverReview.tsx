082baa55129bbcfdb2385b0f09b8e1766285de09
jcoet-gh <johancoet9@gmail.com>
2026-07-17 12:40:59 +0200

    feat: first draft of solver review

--- apps/frontend/src/components/organisms/solver/SolverReview.tsx
+++ apps/frontend/src/components/organisms/solver/SolverReview.tsx
@@ -7,15 +7,37 @@ import {
   CardDescription,
   CardHeader,
 } from "@/components/atoms/baseShadcn/card";
+import CustomiseEventPanel from "@/components/atoms/customise/CustomiseEventPanel";
+import { EventResponse } from "@/app/builder/utils/events/eventRequestBuilder";
+import { ModuleResponseDto } from "@/app/builder/utils/modules/requestBuilders";
+import { Button } from "@/components/atoms/baseShadcn/button";
 
-export default function SolverReview() {
+interface SolverReviewProps {
+  modules: ModuleResponseDto[];
+  events: EventResponse[];
+}
+
+export default function SolverReview({ events, modules }: SolverReviewProps) {
   return (
     <>
-      <Card>
-        <CardHeader>Review</CardHeader>
-        <CardContent>
-          Review your stuff here cuz
-          <CardDescription>Very descriptive</CardDescription>
+      <Card className="shadow-lg border-[var(--border)] rounded-xl bg-[var(--bg-surface)]">
+        <CardHeader className="text-xl font-bold text-[var(--text-primary)]">
+          Review your activities
+        </CardHeader>
+        <CardDescription className="px-6">
+          Review your parsed activities before moving to preferences
+        </CardDescription>
+        <CardContent className="space-y-4">
+          {events?.map((event) => {
+            return (
+              <CustomiseEventPanel
+                key={event.eventID}
+                event={event}
+                modules={modules}
+              />
+            );
+          }) || <p>No event found</p>}
+          <Button type="button">Set Preferences</Button>
         </CardContent>
       </Card>
     </>
--- apps/frontend/src/components/templates/solver/SolverShell.tsx
+++ apps/frontend/src/components/templates/solver/SolverShell.tsx
@@ -4,6 +4,80 @@ import { WizardStepper } from "@/components/atoms/builder/WizardStepper";
 import SolverUpload from "@/components/organisms/solver/SolverUpload";
 import SolverReview from "@/components/organisms/solver/SolverReview";
 import SolverPreferences from "@/components/organisms/solver/SolverPreferences";
+import { ModuleResponseDto } from "@/app/builder/utils/modules/requestBuilders";
+import { EventResponse } from "@/app/builder/utils/events/eventRequestBuilder";
+
+const events: EventResponse[] = [
+  {
+    eventCriteria: {
+      date: "2026/06/07",
+      endTime: "9:00",
+      startTime: "10:00",
+      moduleID: "301",
+      type: "university",
+      venue: "IT-2-26",
+    },
+    eventID: "1",
+    isRecurring: false,
+    eventCode: "COS301",
+    eventName: "Software Engineering",
+  },
+  {
+    eventCriteria: {
+      date: "2026/06/07",
+      endTime: "9:00",
+      startTime: "10:00",
+      moduleID: "332",
+      type: "university",
+      venue: "IT-2-27",
+    },
+    eventID: "2",
+    isRecurring: false,
+    eventCode: "COS332",
+    eventName: "Networks",
+  },
+  {
+    eventCriteria: {
+      date: "2026/06/07",
+      endTime: "9:00",
+      startTime: "10:00",
+      moduleID: "333",
+      type: "university",
+      venue: "IT-2-24",
+    },
+    eventID: "3",
+    isRecurring: false,
+    eventCode: "COS333",
+    eventName: "Software Engineering",
+  },
+];
+
+const modules: ModuleResponseDto[] = [
+  {
+    moduleCode: "301",
+    moduleID: "COS301",
+    moduleName: "Software Engineering",
+    moduleDescription: "",
+    ModuleGroupingID: "",
+    styling: { colour: "blue" },
+  },
+  {
+    moduleCode: "332",
+    moduleID: "COS332",
+    moduleName: "Networks",
+    moduleDescription: "",
+    ModuleGroupingID: "",
+    styling: { colour: "red" },
+  },
+  {
+    moduleCode: "333",
+    moduleID: "COS333",
+    moduleName: "Programming Languages",
+    moduleDescription: "",
+    ModuleGroupingID: "",
+    styling: { colour: "green" },
+  },
+];
 
 export default function SolverShell() {
   return (
@@ -23,7 +97,7 @@ export default function SolverShell() {
           <SolverUpload />
         </div>
         <div>
-          <SolverReview />
+          <SolverReview events={events} modules={modules} />
         </div>
         <div>
           <SolverPreferences />
