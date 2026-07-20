1f1000f297e35e46ab561d16367d198891206edc
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-07-20 10:40:14 +0200

    feat(frontend): started with help menu

---
+++ apps/frontend/src/components/organisms/nav/HelpCommandPallete.tsx
@@ -0,0 +1,13 @@
+// "use client";
+
+// import { useState, useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import CommandPalette from "react-cmdk";
+// import "react-cmdk/dist/cmdk.css";
+
+// export function HelpCommandPalette() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [search, setSearch] = useState("");
+//   const router = useRouter();
+
+// }
