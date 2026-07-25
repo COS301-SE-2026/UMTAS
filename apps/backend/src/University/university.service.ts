e23b23e440901098588d48cfa7907317ac987263
sdcreek240 <aidandwsn@gmail.com>
2026-07-25 09:39:01 +0200

    fix(roles): cleaned up roles validateSession

--- apps/backend/src/University/university.service.ts
+++ apps/backend/src/University/university.service.ts
@@ -68,7 +68,7 @@ export class UniversityService {
   ): Promise<UniversityListResponseDto> {
     const db = tx ?? this.dbService.db;
 
-    console.log(`Hierso: ${userId}`);
+    // console.log(`Hierso: ${userId}`);
     const universities = await db
       .select({
         UniversityID: University.UniversityID,
@@ -85,7 +85,7 @@ export class UniversityService {
       )
       .where(notLike(University.UniversityName, 'user%'));
 
-    console.log(`Meneer: ${JSON.stringify(universities)}`);
+    // console.log(`Meneer: ${JSON.stringify(universities)}`);
     //@Aidan - this is where you f***ed up
 
     // .where(
