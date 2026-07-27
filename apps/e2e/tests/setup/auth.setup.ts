9b62aacbab59b2b7c3644ee34f723485fe777215
Wilmar-Smit <u24584216@tuks.co.za>
2026-07-27 11:45:00 +0200

    test(e2e): added a create module test

--- apps/e2e/tests/setup/auth.setup.ts
+++ apps/e2e/tests/setup/auth.setup.ts
@@ -5,10 +5,10 @@ const authFile = "playwright/.auth/user.json";
 test("authenticate Admin", async ({ page }) => {
   await page.goto("/login");
   await page
-    .getByLabel("Email")
+    .getByLabel("Email", { exact: true })
     .fill(process.env.SEED_SYSTEM_ADMIN_EMAIL ?? "system-admin@local.umtas");
   await page
-    .getByLabel("Password")
+    .getByLabel("Password", { exact: true })
     .fill(process.env.SEED_SYSTEM_ADMIN_PASSWORD ?? "Admin@UMTAS2024!");
 
   await page.getByRole("button", { name: "Log in" }).click();
