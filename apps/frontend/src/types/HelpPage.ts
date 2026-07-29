247e6f6b1ba4e734aa5e5d0ad579463bb7745278
jcoet-gh <johancoet9@gmail.com>
2026-07-29 23:04:24 +0200

    fix: readded course add button

--- apps/frontend/src/types/HelpPage.ts
+++ apps/frontend/src/types/HelpPage.ts
@@ -12,7 +12,8 @@ export interface HelpPageSection {
   description: string;
   pageImage: {
     url: string;
-    altText: string;
+    altText?: string;
+    imageDescription?: string;
   };
   steps: HelpStep[];
   relatedPageIds?: string[]; //link to the actual page (router.push(/page))
