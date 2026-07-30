2b7faf8c64649912d4366605b74e124d4d3f0880
sdcreek240 <aidandwsn@gmail.com>
2026-07-30 14:55:52 +0200

    feat(HelpPage): helpPage for event builder implemented

---
+++ apps/frontend/src/app/builder/HelpPage.ts
@@ -0,0 +1,55 @@
+import {
+  HelpPageSection,
+  HelpStep,
+  createHelpPage,
+} from "../../types/HelpPage";
+
+const page = "Event Builder";
+
+const steps: HelpStep[] = [
+  {
+    stepNumber: 1,
+    title: "Create a module",
+    description: `Click the 'Add Module' button to create a new default module that belongs to you.`,
+  },
+  {
+    stepNumber: 2,
+    title: "Edit a module",
+    description: `To edit a module, just click on the module, and you will be provided with the fields you can modify on the module after which you can just click 'Confirm'.`,
+  },
+  {
+    stepNumber: 3,
+    title: "Go to Events",
+    description: `To create/modify events, click the 'Next: Events' button to go to the relevant page`,
+  },
+  {
+    stepNumber: 4,
+    title: "Create an event",
+    description: `Create a new default event by clicking the 'Add Event' button.`,
+  },
+  {
+    stepNumber: 5,
+    title: "Edit an event",
+    description: `To edit an event, just click on the event, and you will be provided with the fields you can modify on the event after which you can just click 'Confirm'`,
+  },
+  {
+    stepNumber: 6,
+    title: "Go to the Create Schedule",
+    description: `To create your schedule, click the 'Create Timetable' button to go to the schedules page to create your schedule.`,
+  },
+  {
+    stepNumber: 7,
+    title: "Review and generate",
+    description: `Select and customise the events you want to add to your schedule, also provide a name for your schedule.`,
+  },
+  {
+    stepNumber: 8,
+    title: `Create your schedule`,
+    description: `After selecting at least one event, you can click the 'Generate Schedule' button to create your custom schedule.`,
+  },
+];
+
+export const dashboardHelpPage: HelpPageSection = createHelpPage(page, {
+  roles: "all",
+  steps,
+});
