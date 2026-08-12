0b2208bdbb69d475f067dc09831d54ff949b1d93
Wilmar-Smit <u24584216@tuks.co.za>
2026-08-12 09:08:00 +0200

    feat(heursitics): renamed files for efficiency

--- apps/preference-solver/src/heuristic/Decorators/Decorators.cpp
+++ apps/preference-solver/src/heuristic/Decorators/Decorators.cpp
@@ -19,3 +19,18 @@ double TargetStartTime::calculateHeursitic(EventChromosome events) {
 
   return MAD + H_Decorator::calculateHeursitic(events); // this calls next
 }
+
+#include "Decorators.h"
+
+double SkipDayDec::calculateHeursitic(EventChromosome events) {
+  std::cout << "Skip day heuristic ran" << std::endl;
+  double score = 0;
+  for (const EventGA &event : events.events) {
+    if (event.dayOfWeek == day) {
+      score += 100;
+    }
+  }
+  return score + H_Decorator::calculateHeursitic(
+                     events); // for every event on the same day it theres a
+                              // penalty applied
+}
--- apps/preference-solver/src/heuristic/Handlers/getHeuristic.cpp
+++ apps/preference-solver/src/heuristic/Handlers/getHeuristic.cpp
@@ -14,3 +14,19 @@ BaseHeuristic *SkipDayHandler::getHeuristic(nlohmann::json input) {
   } else
     return Handler::getHeuristic(input); // does the null check
 }
+
+BaseHeuristic *TargetStartTimeHandler::getHeuristic(nlohmann::json input) {
+
+  json params = this->getParams(input);
+  if (params != nullptr) {
+    int targetTime = 0;
+
+    if (params.contains(targetKey) && params[targetKey].is_number_integer()) {
+
+      targetTime = params[targetKey].get<int>();
+    }
+    return new TargetStartTime(targetTime);
+
+  } else
+    return Handler::getHeuristic(input); // does the null check
+}
