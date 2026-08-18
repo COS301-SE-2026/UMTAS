4352eda21d820c11fd2b41e7ff6dbbaf35788e38
jcoet-gh <johancoet9@gmail.com>
2026-08-18 18:56:38 +0200

    feat: added the request builder for buildings

---
+++ apps/frontend/utilities/building/buildingRequestBuilder.ts
@@ -0,0 +1,35 @@
+import { RequestBuilder, RequestMethod } from "../request";
+import { paths } from "@/lib/api";
+
+export type getAllBuildingsPath =
+  paths["/api/buildings"]["get"]["parameters"]["path"];
+export type getAllBuildingsRes =
+  paths["/api/buildings"]["get"]["responses"]["200"]["content"]["application/json"];
+
+export type createBuildingBody =
+  paths["/api/buildings"]["post"]["requestBody"]["content"]["application/json"];
+export type createBuildingPath =
+  paths["/api/buildings"]["post"]["parameters"]["path"];
+export type createBuildingRes =
+  paths["/api/buildings"]["post"]["responses"]["201"]["content"]["application/json"];
+
+export class getAllBuildingsBuilder extends RequestBuilder<
+  getAllBuildingsPath,
+  getAllBuildingsRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/buildings").setMethod(RequestMethod.GET);
+  }
+}
+
+export class createBuildingBuilder extends RequestBuilder<
+  createBuildingBody,
+  createBuildingPath,
+  createBuildingRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/buildings").setMethod(RequestMethod.POST);
+  }
+}
