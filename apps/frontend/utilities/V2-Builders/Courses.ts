60866f601039dafa8a084cf58cd0efc5f14b29d9
Wilmar-Smit <u24584216@tuks.co.za>
2026-08-19 10:24:04 +0200

    feat(v2): added the v2 get courses ky

--- apps/frontend/utilities/V2-Builders/Courses.ts
+++ apps/frontend/utilities/V2-Builders/Courses.ts
@@ -1,1 +1,23 @@
-import { paths } from "@/lib/api";
+import { api } from "@/components/tanstack/getQueryClient";
+import { components, paths } from "@/lib/api";
+
+export type getCoursesV2 = paths["/api/Courses/v2/getAll"]["get"];
+export type getCoursesV2Params = getCoursesV2["parameters"]["query"];
+export type getCoursesV2Resp =
+  getCoursesV2["responses"]["200"]["content"]["application/json"];
+
+export type CourseDtoV2 = components["schemas"]["CourseDto"];
+
+type paramsCoursesV2 =
+  | { UniversityID?: string; CourseName?: string; Degree?: string }
+  | undefined;
+
+export async function getAllCoursesV2(
+  params: getCoursesV2Params,
+): Promise<getCoursesV2Resp> {
+  return api
+    .get("Courses/v2/getAll", {
+      searchParams: params as paramsCoursesV2,
+    })
+    .json<getCoursesV2Resp>();
+}
