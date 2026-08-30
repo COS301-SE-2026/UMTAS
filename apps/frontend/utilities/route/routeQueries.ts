a28fc3a5908c822818e615b4a3c3b2a316648b35
jcoet-gh <johancoet9@gmail.com>
2026-08-30 12:34:44 +0200

    feat: added the builder and query for active route

--- apps/frontend/utilities/route/routeQueries.ts
+++ apps/frontend/utilities/route/routeQueries.ts
@@ -1,5 +1,10 @@
 import { queryOptions } from "@tanstack/react-query";
-import { getRouteQuery, getRouterBuilder } from "./routeRequestBuilder";
+import {
+  getActiveRouteBuilder,
+  getActiveRouteQuery,
+  getRouteQuery,
+  getRouterBuilder,
+} from "./routeRequestBuilder";
 
 export function getRouteQ(query: getRouteQuery) {
   return queryOptions({
@@ -10,6 +15,18 @@ export function getRouteQ(query: getRouteQuery) {
     ] as const,
     queryFn: async () => {
       const result = (await new getRouterBuilder().send({ paths: query }))
+        .route;
+
+      return result;
+    },
+  });
+}
+
+export function getActiveRouteQ(query: getActiveRouteQuery) {
+  return queryOptions({
+    queryKey: ["routes", '"active', query.date, query.time] as const,
+    queryFn: async () => {
+      const result = (await new getActiveRouteBuilder().send({ paths: query }))
         .route;
 
       return result;
--- apps/frontend/utilities/route/routeRequestBuilder.ts
+++ apps/frontend/utilities/route/routeRequestBuilder.ts
@@ -5,6 +5,11 @@ export type getRouteQuery = paths["/api/routes"]["get"]["parameters"]["query"];
 export type getRouteRes =
   paths["/api/routes"]["get"]["responses"]["200"]["content"]["application/json"];
 
+export type getActiveRouteQuery =
+  paths["/api/routes/active"]["get"]["parameters"]["query"];
+export type getActiveRouteRes =
+  paths["/api/routes/active"]["get"]["responses"]["200"]["content"]["application/json"];
+
 export class getRouterBuilder extends RequestBuilder<
   getRouteQuery,
   undefined,
@@ -13,5 +18,16 @@ export class getRouterBuilder extends RequestBuilder<
   constructor() {
     super();
     this.setUrl("/routes").setMethod(RequestMethod.GET);
+  }
+}
+
+export class getActiveRouteBuilder extends RequestBuilder<
+  getActiveRouteQuery,
+  undefined,
+  getActiveRouteRes
+> {
+  constructor() {
+    super();
+    this.setUrl("/routes/active").setMethod(RequestMethod.GET);
   }
 }
