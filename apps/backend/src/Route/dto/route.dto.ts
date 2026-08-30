906f575765735a1f146c78a0d4f78adef7a3bc30
jcoet-gh <johancoet9@gmail.com>
2026-08-30 11:10:29 +0200

    feat: added new dtos for active routes

--- apps/backend/src/Route/dto/route.dto.ts
+++ apps/backend/src/Route/dto/route.dto.ts
@@ -2,9 +2,13 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsArray,
+  IsDateString,
+  IsEnum,
   IsHexColor,
   IsInt,
+  IsOptional,
   IsPositive,
+  IsString,
   IsUUID,
   ValidateNested,
 } from 'class-validator';
@@ -76,6 +80,28 @@ export class RouteDto {
   displayColour!: string;
 }
 
+export enum ActiveRouteStatus {
+  AT_VENUE = 'AT_VENUE',
+  MOVING = 'MOVING',
+  NONE = 'NONE',
+}
+
+export class ActiveRouteQueryDto {
+  @ApiProperty({
+    description: 'Calendar date that matches the EventAttendance date',
+    example: '2026/10/12',
+  })
+  @IsDateString()
+  date!: string;
+
+  @ApiProperty({
+    description: 'Time in hh:mm',
+    example: '10:12',
+  })
+  @IsString()
+  time!: string;
+}
+
 export class RouteSingleResponseDto {
   @ApiProperty({
     type: RouteDto,
@@ -83,4 +109,48 @@ export class RouteSingleResponseDto {
   @ValidateNested()
   @Type(() => RouteDto)
   route!: RouteDto;
+}
+
+export class ActiveRouteResponseDto {
+  @ApiProperty({
+    enum: ActiveRouteStatus,
+    enumName: 'ActiveRouteStatus',
+    example: ActiveRouteStatus.MOVING,
+  })
+  @IsEnum(ActiveRouteStatus)
+  status!: ActiveRouteStatus;
+
+  @ApiProperty({
+    format: 'uuid',
+    required: false,
+    example: '00000000-0000-0000-0000-000000000000',
+  })
+  @IsUUID()
+  @IsOptional()
+  currentBuildingId?: string;
+
+  @ApiProperty({
+    type: () => RouteDto,
+    required: false,
+  })
+  @ValidateNested()
+  @IsOptional()
+  @Type(() => RouteDto)
+  route?: RouteDto;
+
+  @ApiProperty({
+    example: 'Lecture 1',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  fromEventName?: string;
+
+  @ApiProperty({
+    example: 'Lecture 2',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  toEventName?: string;
 }
