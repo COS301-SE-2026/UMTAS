f63ccc61302a43da65a1994d57ed3756c5f512d3
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-08-31 17:16:53 +0200

    feat(devops): add posthog headers

--- apps/frontend/src/app/posthog/providers.tsx
+++ apps/frontend/src/app/posthog/providers.tsx
@@ -5,8 +5,17 @@ import { PostHogProvider as PHProvider } from "@posthog/react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+    let headers = ["localhost:3001"];
+
+    if (process.env.NEXT_PUBLIC_APP_ENV === "staging") {
+      headers = ["https://staging.capstone-vigil.dns.net.za/api"];
+    } else if (process.env.NEXT_PUBLIC_APP_ENV === "production") {
+      headers = ["https://capstone-vigil.dns.net.za/api"];
+    }
+
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PT as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+      tracing_headers: headers,
     });
   }, []);
 
