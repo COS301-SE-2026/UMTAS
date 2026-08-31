d1227d7436d825d708be83c1fc243b25437aaed1
sdcreek240 <aidandwsn@gmail.com>
2026-08-31 20:09:13 +0200

    chore(seeding): removed nwu from db seeding

--- apps/backend/src/db/seeding/services/university.seed.service.ts
+++ apps/backend/src/db/seeding/services/university.seed.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../../database.service';
 import { BaseSeedService } from '../base.seed.service';
 import { University, usersTable } from '../../../entities';
-import { eq } from 'drizzle-orm';
+import { eq, ilike } from 'drizzle-orm';
 import { SeedPersistenceService } from '../seed-persistence.service';
 import { AppDatabase } from 'src/auth/auth';
 
@@ -61,11 +61,6 @@ export class UniversitySeedService extends BaseSeedService {
             UserID: uniAdmin.id,
             role: 'UNIVERSITY_ADMIN',
           },
-          {
-            UniversityID: uniSeed[2].UniversityID,
-            UserID: uniAdmin.id,
-            role: 'UNIVERSITY_ADMIN',
-          },
         ]);
       }
     } //END_check for missing names
@@ -74,39 +69,16 @@ export class UniversitySeedService extends BaseSeedService {
       this.logResult('Universities');
     }
 
-    await this.NWUmockApi(tx);
     await this.MarylandAPI(tx);
   } //END_seed
 
   //🎅's little helpers
-  async NWUmockApi(tx: AppDatabase) {
-    const nwuName = this.constants.UniversityNames[1];
-
-    const [nwu] = await tx
-      .select()
-      .from(University)
-      .where(eq(University.UniversityName, nwuName))
-      .limit(1);
-
-    //update with api information
-    const apiInfo = {
-      ApiIdentifier: 'NWU',
-      BaseApiUrl: 'http://localhost:3010',
-    };
-
-    await tx
-      .update(University)
-      .set(apiInfo)
-      .where(eq(University.UniversityID, nwu.UniversityID));
-  }
 
   async MarylandAPI(tx: AppDatabase) {
-    const maryName = this.constants.UniversityNames[2];
-
     const [maryland] = await tx
       .select()
       .from(University)
-      .where(eq(University.UniversityName, maryName))
+      .where(ilike(University.UniversityName, `%Maryland%`))
       .limit(1);
 
     //update with api information
