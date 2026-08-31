f4516b6958c679853f073504a5074d88f7681217
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-08-31 22:04:47 +0200

    feat(devops): update seeding

--- apps/backend/src/db/seeding/Constants/Users.constants.ts
+++ apps/backend/src/db/seeding/Constants/Users.constants.ts
@@ -8,17 +8,16 @@ export const UserNames: string[] = [
   'Piet Pierneef',
   'Cos Admin',
 ];
-
 export const UserEmails: string[] = [
   'Jannie Bloekom@FlyAtUP.com',
   'Sarrie Jammer Gat@FlyAtUP.com',
   'Piet Pierneef@FlyAtUP.com',
-  'cos301Admin@gmail.com',
+  process.env.SEED_COS_ADMIN_EMAIL ?? 'Admin@UMTAS2024!',
 ];
 
 export const UserPasswords: string[] = [
   '123Jannie Bloekom#123',
   '123Sarrie Jammer Gat#123',
   '123Piet Pierneef#123',
-  '123COSADMIN#123',
+  process.env.SEED_COS_ADMIN_PASSWORD ?? 'Admin@UMTAS2024!',
 ];
