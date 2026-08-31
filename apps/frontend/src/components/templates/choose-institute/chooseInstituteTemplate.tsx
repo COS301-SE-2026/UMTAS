1306e760c78edf7bec78d118618f32deb30f080b
Wilmar-Smit <u24584216@tuks.co.za>
2026-08-31 09:33:20 +0200

    feat(AC): bug fixes

--- apps/frontend/src/components/templates/choose-institute/chooseInstituteTemplate.tsx
+++ apps/frontend/src/components/templates/choose-institute/chooseInstituteTemplate.tsx
@@ -5,6 +5,7 @@ import {
   CardTitle,
 } from "@/components/atoms/baseShadcn/card";
 import { InstituteSelector } from "@/components/organisms/choose-institute/instituteSelector";
+import { UserDetails } from "@/lib/userclass/userClass";
 
 interface ChooseInstituteTemplateProps {
   onClose?: () => void;
@@ -13,10 +14,14 @@ interface ChooseInstituteTemplateProps {
 export function ChooseInstituteTemplate({
   onClose,
 }: ChooseInstituteTemplateProps) {
+  const uni = UserDetails.getUniDetails();
+
   return (
     <Card className="mx-auto min-w-md bg-[var(--bg-surface)]">
       <CardHeader>
         <CardTitle>Choose Institute</CardTitle>
+        <br />
+        {uni && <>Current University : {uni.UniversityName}</>}
       </CardHeader>
       <CardContent>
         <InstituteSelector onClose={onClose} />
