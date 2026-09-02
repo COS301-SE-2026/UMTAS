80c86c9919798da076124241b407cc7b8740974c
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-02 14:53:42 +0200

    feat(fix): cannot automate the process

--- apps/e2e/tests/lighthouse/builder.audit.ts
+++
@@ -1,8 +0,0 @@
-import test, { Page } from "@playwright/test";
-import { playAudit } from "playwright-lighthouse";
-import { getLhConfig } from "./lighthouse.functions";
-
-test("builder", async ({ page }) => {
-  await page.goto("/builder");
-  await playAudit(getLhConfig(page));
-});
