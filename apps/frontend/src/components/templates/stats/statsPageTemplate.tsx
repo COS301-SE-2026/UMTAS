9935ea64a6aa7fdacaeb2b18949dddeca103e584
jcoet-gh <johancoet9@gmail.com>
2026-09-02 14:40:49 +0200

    feat: finished the template for the stats page

---
+++ apps/frontend/src/components/templates/stats/statsPageTemplate.tsx
@@ -0,0 +1,71 @@
+import UniversityStats, {
+  UniversityStatsData,
+} from "@/components/organisms/stats/universityTab";
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/atoms/baseShadcn/tabs";
+
+const mockUniversityStats: UniversityStatsData = {
+  countCourses: 100,
+  countEvents: 67,
+  countModule: 420,
+  numberOfStudents: 69690,
+};
+
+export default async function statsPageTemplate() {
+  return (
+    <div className="container mx-auto py-10 space-y-6">
+      <div>
+        <h1 className="text-2xl font-bold tracking-tight text-[var(--text-primary)] pb-2">
+          Stats Dashboard
+        </h1>
+        <p className="text-sm text-[var(--text-secondary)]">
+          View various statistics and metrics related to the UMTAS system.
+        </p>
+      </div>
+
+      <div>
+        <Tabs defaultValue="university" className="w-full space-y-4">
+          <TabsList className="w-full flex flex-wrap gap-2 h-auto p-2">
+            <TabsTrigger value="university" className="px-4 py-2">
+              University
+            </TabsTrigger>
+            <TabsTrigger value="courses" className="px-4 py-2">
+              Courses
+            </TabsTrigger>
+            <TabsTrigger value="events" className="px-4 py-2">
+              Events
+            </TabsTrigger>
+            <TabsTrigger value="modules" className="px-4 py-2">
+              Modules
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="university" className="space-y-6">
+            <UniversityStats
+              data={mockUniversityStats}
+              isLoading={false}
+              isError={false}
+            />
+          </TabsContent>
+
+          <TabsContent value="courses">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+          </TabsContent>
+
+          <TabsContent value="events">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+          </TabsContent>
+
+          <TabsContent value="modules">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+}
