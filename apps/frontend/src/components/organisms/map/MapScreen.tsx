0c68ee05c8d3c94778e4e795bf4e9d674419b4c6
marcelstoltz00 <marcelstoltz00@gmail.com>
2026-09-03 11:14:26 +0200

    fix(docs): fixed dockerfiles for map key

--- apps/frontend/src/components/organisms/map/MapScreen.tsx
+++ apps/frontend/src/components/organisms/map/MapScreen.tsx
@@ -78,7 +78,7 @@ export function MapScreen({ children, onRequestMapSetup }: MapScreenProps) {
   return (
     <Map
       style={mapStyle}
-      mapId={process.env.NEXT_PUBLIC_MAP_KEY || "DEMO_MAP_ID"} //should work
+      mapId={process.env.NEXT_PUBLIC_MAP_KEY} //should work
       defaultBounds={mapRestriction?.latLngBounds}
       defaultZoom={config.DefaultZoom}
       restriction={mapRestriction}
