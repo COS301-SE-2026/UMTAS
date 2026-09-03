13b891ae7e18acd68ce25209861cebc6b73c0b6c
jcoet-gh <johancoet9@gmail.com>
2026-09-03 09:47:33 +0200

    fix: fixed lowercase in ui

--- apps/frontend/src/components/molecules/viewTimetable/EventBlock.tsx
+++ apps/frontend/src/components/molecules/viewTimetable/EventBlock.tsx
@@ -45,7 +45,7 @@ export function EventBlock({ event, date }: EventBlockProps) {
       window.dispatchEvent(
         new CustomEvent(errorName, {
           detail: {
-            userMessage: `Event: ${event.name} : updated to ${attending}`,
+            userMessage: `Event: ${event.name} : Updated to ${attending}`,
           },
         }),
       );
