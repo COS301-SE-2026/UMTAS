f83b23c57012a57e68c61080ed8246107cf1c482
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-10 08:55:49 +0200

    feat(VM): added a constructor for VM

--- apps/frontend/wasm-engine/src/lib.rs
+++ apps/frontend/wasm-engine/src/lib.rs
@@ -1,1 +1,13 @@
+use tract::prelude::*;
 use wasm_bindgen::prelude::*;
+tract::impl_ndarray_interop!();
+
+#[wasm_bindgen]
+pub struct YoloWasmEngine {
+    model: Runnable,
+}
+
+#[wasm_bindgen]
+impl YoloWasmEngine {
+    pub fn new(modelBytes: &[u8]) -> Result<YoloWasmEngine, JsValue> {}
+}
