f231cf9760be7dd916cecfffef6fc593db4c5293
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-11 13:29:08 +0200

    test(cal-e2e): added single date restriction test

--- apps/e2e/tests/CalendarManagement/CalManage.spec.ts
+++ apps/e2e/tests/CalendarManagement/CalManage.spec.ts
@@ -27,11 +27,46 @@ test("Calendar Page add Calendar year", async ({ page }) => {
   await expect(page.getByText("2030")).toBeVisible();
 });
 
-test("Calendar Page add Calendar year", async ({ page }) => {
+test("Calendar Page add Restriction holiday", async ({ page }) => {
   await page.goto("/calendar-management");
+  await page.getByTestId("SELECT_NEW_YEAR").click();
+  await page.getByRole("option", { name: "2030" }).click();
+  await expect(page.getByText("2030")).toBeVisible();
 
   const SingleDateType: ResTypes = "PUBLIC_HOLIDAY";
 
   await page.getByTestId(`CREATE_RESTRICTION`).click();
   await page.getByTestId(`MENU_ITEM_${SingleDateType}`).click();
+
+  await expect(
+    page.getByTestId("TEMP_CONTAINER").getByTestId("restriction-Date-Input"),
+  ).toBeVisible();
+
+  await expect(
+    page.getByTestId("TEMP_CONTAINER").getByTestId("restriction-dsc-Input"),
+  ).toBeVisible();
+
+  await page
+    .getByTestId("TEMP_CONTAINER")
+    .getByTestId("restriction-Date-Input")
+    .fill("2030-12-30");
+
+  await page
+    .getByTestId("TEMP_CONTAINER")
+    .getByTestId("restriction-dsc-Input")
+    .fill("Test Holiday");
+
+  await page
+    .getByTestId("TEMP_CONTAINER")
+    .getByTestId("btn-save-restriction")
+    .click();
+
+  await expect(
+    page.getByTestId("ADDED_CONTAINER").getByTestId("restriction-dsc-Input"),
+  ).toHaveValue("Test Holiday");
+
+  await page
+    .getByTestId("ADDED_CONTAINER")
+    .getByTestId("btn-delete-restriction")
+    .click();
 });
