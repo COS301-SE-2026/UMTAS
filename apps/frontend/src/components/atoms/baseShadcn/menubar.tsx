c58f33326131068211cd29465daa69a4d34c957a
jcoet-gh <johancoet9@gmail.com>
2026-09-11 18:00:20 +0200

    chore: improved the navbar

--- apps/frontend/src/components/atoms/baseShadcn/menubar.tsx
+++ apps/frontend/src/components/atoms/baseShadcn/menubar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { cn } from "cn";
+import { cn } from "@/../utilities/utils";
 import { Menubar as MenubarPrimitive } from "radix-ui";
 import { CheckIcon, ChevronRightIcon } from "lucide-react";
 
