ad89304fc1bee73e78c1c7de3bee082884060d3f
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-12 17:37:48 +0200

    feat(VisionModel): added quadrants for slicing

--- apps/frontend/wasm-engine/src/lib.rs
+++ apps/frontend/wasm-engine/src/lib.rs
@@ -1,5 +1,26 @@
-use std::usize;
+use std::fmt::Alignment::{Center, Right};
 
 use wasm_bindgen::prelude::*;
 
-pub fn slice_image_data(pixel_data: &[u8], width: usize, height: usize) -> js_sys::Array {}
+struct SliceFormat {
+    x: usize,
+    y: usize,
+}
+
+#[wasm_bindgen]
+pub fn slice_image_data(pixel_data: &[u8], width: usize, height: usize) -> js_sys::Array {
+    let center_x = width / 2;
+    let center_y = height / 2;
+
+    let result = js_sys::Array::new();
+
+    let topLeft = SliceFormat { x: 0, y: 0 };
+    let topMiddle = SliceFormat { x: center_x, y: 0 };
+    let middleLeft = SliceFormat { x: 0, y: center_y };
+    let center = SliceFormat {
+        x: center_x,
+        y: center_y,
+    };
+
+    return result;
+}
