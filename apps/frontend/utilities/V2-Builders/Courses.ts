ef3269fbb6641a6304e1361e22995dfb55c39812
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-12 14:45:24 +0200

    feat(Courses): adding builders and mutations

--- apps/frontend/utilities/V2-Builders/Courses.ts
+++ apps/frontend/utilities/V2-Builders/Courses.ts
@@ -1,5 +1,7 @@
-import { api } from "@/components/tanstack/getQueryClient";
+import { api, getQueryClient } from "@/components/tanstack/getQueryClient";
 import { components, paths } from "@/lib/api";
+import { RequestBuilder, RequestMethod } from "../request";
+import { mutationOptions } from "@tanstack/react-query";
 
 export type getCoursesV2 = paths["/api/Courses/v2/getAll"]["get"];
 export type getCoursesV2Params = getCoursesV2["parameters"]["query"];
@@ -19,4 +21,60 @@ export async function getAllCoursesV2(
       searchParams: params as paramsCoursesV2,
     })
     .json<getCoursesV2Resp>();
+}
+
+export type enrollCourse =
+  paths["/api/Courses/course-enrollment/{CourseId}"]["post"];
+
+export type enrollCourseParam = enrollCourse["parameters"]["path"];
+export type enrollCourseResp =
+  enrollCourse["responses"]["201"]["content"]["application/json"];
+
+export class enrollCourseBuilder extends RequestBuilder<
+  enrollCourseParam,
+  undefined,
+  enrollCourseResp
+> {
+  constructor() {
+    super();
+    this.setUrl("/Courses/course-enrollment/{CourseId}").setMethod(
+      RequestMethod.POST,
+    );
+  }
+}
+
+export type deleteEnrollmentCourse =
+  paths["/api/Courses/course-unenrollment/{CourseId}"]["delete"];
+
+export type deleteEnrollmentCourseParam =
+  deleteEnrollmentCourse["parameters"]["path"];
+
+export type deleteEnrollmentCourseResp =
+  deleteEnrollmentCourse["responses"]["200"]["content"]["application/json"];
+
+export class delEnrollCourseBuilder extends RequestBuilder<
+  deleteEnrollmentCourseParam,
+  undefined,
+  deleteEnrollmentCourseResp
+> {
+  constructor() {
+    super();
+    this.setUrl("/Courses/course-unenrollment/{CourseId}").setMethod(
+      RequestMethod.POST,
+    );
+  }
+}
+
+export function enrollUser() {
+  return mutationOptions({
+    mutationFn: async (paths: enrollCourseParam) => {
+      const result = await new enrollCourseBuilder().send({ paths: paths });
+      return result;
+    },
+    onSuccess: () => {
+      getQueryClient().invalidateQueries({
+        queryKey: ["courses"],
+      });
+    },
+  });
 }
