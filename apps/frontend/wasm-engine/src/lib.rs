abc0a57ae2cc3d6b42e3145f075695827f27f05a
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-12 17:48:58 +0200

    feat(VisionModel): added slice widths and heights

--- apps/frontend/wasm-engine/src/lib.rs
+++ apps/frontend/wasm-engine/src/lib.rs
@@ -5,6 +5,8 @@ use wasm_bindgen::prelude::*;
 struct SliceFormat {
     x: usize,
     y: usize,
+    slice_height: usize,
+    slice_width: usize,
 }
 
 #[wasm_bindgen]
@@ -13,14 +15,40 @@ pub fn slice_image_data(pixel_data: &[u8], width: usize, height: usize) -> js_sy
     let center_y = height / 2;
 
     let result = js_sys::Array::new();
+    // can be made to overlap but right now no
+    let slice_height = center_x;
+    let slice_width = center_y;
+
     // the slices
-    let top_left = SliceFormat { x: 0, y: 0 };
-    let top_right = SliceFormat { x: center_x, y: 0 };
-    let bottom_left = SliceFormat { x: 0, y: center_y };
+    let top_left = SliceFormat {
+        x: 0,
+        y: 0,
+        slice_height: slice_height,
+        slice_width: slice_width,
+    };
+    let top_right = SliceFormat {
+        x: center_x,
+        y: 0,
+        slice_height: slice_height,
+        slice_width: slice_width,
+    };
+    let bottom_left = SliceFormat {
+        x: 0,
+        y: center_y,
+        slice_height: slice_height,
+        slice_width: slice_width,
+    };
     let bottom_right = SliceFormat {
         x: center_x,
         y: center_y,
+        slice_height: slice_height,
+        slice_width: slice_width,
     };
 
     return result;
+}
+
+struct extract_slice_struct<'a> {
+    start: SliceFormat,
+    pixel_data: &'a [u8], // lifetime scary
 }
