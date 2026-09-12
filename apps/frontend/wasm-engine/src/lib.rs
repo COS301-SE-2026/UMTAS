8fc96d56eb26049dc9cb871f9ab33a9b4e3c43be
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-12 18:13:53 +0200

    feat(VisionModel): getting to pixel data now

--- apps/frontend/wasm-engine/src/lib.rs
+++ apps/frontend/wasm-engine/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::Alignment::{Center, Right};
+use std::usize;
 
 use wasm_bindgen::prelude::*;
 
@@ -16,8 +16,8 @@ pub fn slice_image_data(pixel_data: &[u8], width: usize, height: usize) -> js_sy
 
     let result = js_sys::Array::new();
     // can be made to overlap but right now no
-    let slice_height = center_x;
-    let slice_width = center_y;
+    let slice_height = center_y;
+    let slice_width = center_x;
 
     // the slices
     let top_left = SliceFormat {
@@ -47,8 +47,16 @@ pub fn slice_image_data(pixel_data: &[u8], width: usize, height: usize) -> js_sy
 
     return result;
 }
+// all slices must be broken into [all reds][all greens][all blues] for BCHW
+pub fn extract_slice(start_pix: SliceFormat, pixel_data: &[u8], fullWidth: usize) {
+    let total_pix = start_pix.slice_width * start_pix.slice_height * 3;
 
-struct extract_slice_struct<'a> {
-    start: SliceFormat,
-    pixel_data: &'a [u8], // lifetime scary
+    let mut slice = vec![0.0; total_pix];
+
+    let r_offset = 0;
+    
+}
+
+pub fn normalize_pixel(colour: &u8) -> f32 {
+    return (*colour as f32) / 255.0;
 }
