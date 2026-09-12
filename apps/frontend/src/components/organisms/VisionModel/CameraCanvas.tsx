472d5d037b66e68d358263a2a07d4c66347679d7
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-12 15:17:53 +0200

    feat(VisionModel): removing library webcam

--- apps/frontend/src/components/organisms/VisionModel/CameraCanvas.tsx
+++ apps/frontend/src/components/organisms/VisionModel/CameraCanvas.tsx
@@ -3,62 +3,18 @@ import { Button } from "@/components/atoms/baseShadcn/button";
 import { useRef, useState } from "react";
 import { CircleX } from "lucide-react";
 import Webcam from "react-webcam";
+import { boolean } from "better-auth";
 
 export default function CameraCanvas() {
   const webcamRef = useRef<Webcam>(null);
   const [cameraOn, setCameraOn] = useState(true);
   const [CameraLoading, setCameraLoading] = useState(false);
-  function getPixelData() {
-    const videoNode = webcamRef.current?.video;
-    if (!videoNode || videoNode.readyState < videoNode.HAVE_CURRENT_DATA) {
-      console.log("Video stream not ready yet.");
-      return;
-    }
-
-    const canvas = document.createElement("canvas");
-    canvas.width = videoNode.width || 640;
-    canvas.height = videoNode.height || 480;
-
-    const canvasContext = canvas.getContext("2d");
-    if (!canvasContext) {
-      return;
-    }
-
-    canvasContext.drawImage(videoNode, 0, 0, canvas.width, canvas.height);
-
-    //  [R, G, B, A, R, G, B, A...])
-    const imageData = canvasContext.getImageData(
-      0,
-      0,
-      canvas.width,
-      canvas.height,
-    );
-    console.log("Image Data Object:", imageData);
-    console.log("Total Pixels:", imageData.width * imageData.height);
-    console.log(
-      "First Pixel (RGBA):",
-      imageData.data[0],
-      imageData.data[1],
-      imageData.data[2],
-      imageData.data[3],
-    );
-  }
 
   return (
     <div className="w-full h-full justify-around flex flex-col gap-y-4 p-4">
       <div className="w-full h-full flex flex-col justify-center items-center text-center border rounded-xl ">
         {cameraOn ? (
-          <div className="">
-            <Webcam
-              audio={false}
-              ref={webcamRef}
-              onUserMedia={() => {
-                setCameraLoading(false);
-              }}
-              videoConstraints={{ width: 640, height: 480, facingMode: "user" }}
-              className="w-full h-full"
-            />
-          </div>
+          <div className="">{/* canvas go here */}</div>
         ) : (
           <p
             className="flex gap-x-2"
@@ -73,7 +29,6 @@ export default function CameraCanvas() {
         )}
       </div>
       <div className="flex flex-row justify-around">
-        <Button onClick={getPixelData}>Log screen data</Button>
         <Button
           onClick={() => {
             setCameraOn(!cameraOn);
@@ -84,5 +39,21 @@ export default function CameraCanvas() {
         </Button>
       </div>
     </div>
+  );
+}
+
+interface CanvasCamProps {
+  isCameraActive: boolean;
+}
+
+function CanvasWebcam() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  return (
+    <>
+      <video ref={videoRef} playsInline muted></video>
+      <canvas ref={canvasRef} width={640} height={640}></canvas>
+    </>
   );
 }
