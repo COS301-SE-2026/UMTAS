61c1d585a817453a3ca31229cea642812996fdb8
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-12 20:49:05 +0200

    feat(VisionModel): function cleanup

--- apps/frontend/utilities/VisionModel/modelWorker.ts
+++ apps/frontend/utilities/VisionModel/modelWorker.ts
@@ -1,23 +1,49 @@
-import { DETECT_MESSAGE } from "./messageTypes";
+import { DETECT_MESSAGE, PIXEL_PAYLOAD } from "./messageTypes";
 import init, { slice_image_data } from "../../wasm-engine/pkg/wasm_engine";
+import * as ort from "onnxruntime-web";
+
+let wasmLoaded = false;
+let DetectSession: ort.InferenceSession | null = null;
+
+async function initWasm() {
+  await init();
+  wasmLoaded = true;
+}
+async function initDetection() {
+  const fullUrl = `${location.origin}/models/yolov11n.onnx`;
+
+  DetectSession = await ort.InferenceSession.create(fullUrl, {
+    executionProviders: ["wgpu", "wasm"],
+  });
+}
+
+function createSlices(payload: PIXEL_PAYLOAD) {
+  const wasmPixels = new Uint8Array(
+    payload.pixelData.buffer,
+    payload.pixelData.byteOffset,
+    payload.pixelData.byteLength,
+  );
+
+  return slice_image_data(
+    wasmPixels,
+    payload.width,
+    payload.height,
+  ) as Float32Array[];
+}
 
 self.onmessage = async (event: MessageEvent) => {
   const message = event.data as DETECT_MESSAGE;
 
   if (message.eventType === "DETECT") {
     const payload = message.payload;
-    await init();
+    if (!wasmLoaded) {
+      await initWasm();
+    }
 
-    const wasmPixels = new Uint8Array(
-      payload.pixelData.buffer,
-      payload.pixelData.byteOffset,
-      payload.pixelData.byteLength,
-    );
+    if (!DetectSession) {
+      await initDetection();
+    }
 
-    const slices = slice_image_data(
-      wasmPixels,
-      payload.width,
-      payload.height,
-    ) as Float32Array[];
+    const slices = createSlices(payload);
   }
 };
