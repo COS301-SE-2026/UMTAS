58f1852baa9d3fb6bb58cb4ba79a151a92c3fbb8
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-14 12:03:24 +0200

    feat(VisionModel): its making sense

--- apps/frontend/utilities/VisionModel/detectionManager.ts
+++ apps/frontend/utilities/VisionModel/detectionManager.ts
@@ -1,3 +1,5 @@
+import { DETECT_DATA_MESSAGE, MessageType } from "./messageTypes";
+
 class DetectionManager {
   private worker: Worker | null = null;
   private isProcessing = false;
@@ -8,5 +10,26 @@ class DetectionManager {
     });
   }
 
-  public run(PixelData: Uint8ClampedArray, width: number, height: number) {}
+  public run(
+    PixelData: Uint8ClampedArray,
+    width: number,
+    height: number,
+  ): Promise<Float32Array[] | null> {
+    if (this.isProcessing || !this.worker) {
+      return Promise.resolve(null);
+    }
+    this.isProcessing = true;
+
+    return new Promise((resolve) => {
+      const handleMessage = (event: MessageEvent) => {
+        const type = event.data.type as MessageType;
+        if (type === "DETECT_DATA") {
+          const message = event.data as DETECT_DATA_MESSAGE;
+          this.worker?.removeEventListener("message", handleMessage);
+          this.isProcessing = false;
+          resolve(message.payload.results);
+        }
+      };
+    });
+  }
 }
