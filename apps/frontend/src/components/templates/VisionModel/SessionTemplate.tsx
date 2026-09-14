be57b7cb3cb27410a0e59dc4c68291a0db26a3c0
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-14 15:22:35 +0200

    feat(VisionModel): added inference settings

--- apps/frontend/src/components/templates/VisionModel/SessionTemplate.tsx
+++ apps/frontend/src/components/templates/VisionModel/SessionTemplate.tsx
@@ -27,7 +27,7 @@ export default function VM_SessionTemplate() {
           Session Camera
         </h1>
         <div className="flex flex-row border justify-around w-full h-full ">
-          <div className="flex justify-center w-full h-full items-center">
+          <div className="flex justify-center w-full h-full items-center rounded-2xl rounded-t-none">
             <div className="flex justify-center items-center w-3/4 h-3/4  ">
               <CameraCanvas
                 isCameraActive={cameraOn}
@@ -35,15 +35,15 @@ export default function VM_SessionTemplate() {
               />
             </div>
           </div>
-          <div className="  items-center flex flex-col  w-full max-w-[20vw]">
+          <div className="   flex flex-col  w-full max-w-[20vw] h-full rounded-2xl rounded-t-none">
             {/* ------------ Settings hier asseblief --------------- */}
             <div className="w-full h-full overflow-auto  rounded-t-none  border  border-[var(--border)]  bg-[var(--bg-surface)] shadow-sm flex flex-col">
-              <div className="flex flex-col gap-5    min-h-[10vh] justify-center w-full h-full text-center p-2">
-                <div className="h-full flex flex-col  gap-y-5 p-2 ">
+              <div className="flex flex-col gap-5    min-h-[10vh]  w-full h-full  p-2">
+                <div className="h-2/10 flex flex-col w-full gap-y-5 p-2 ">
                   <h1 className="text-md font-semibold  text-[var(--text-primary)] pl-4 py-4 ">
                     Camera Settings
                   </h1>
-                  <Label className=" flex flex-col  w-full justify-center text-md font-medium text-[var(--text-primary)] text-left pl-1">
+                  <Label className=" flex flex-col  w-full text-md font-medium text-[var(--text-primary)] text-left pl-1">
                     Camera On
                     <Switch
                       checked={cameraOn}
@@ -53,12 +53,12 @@ export default function VM_SessionTemplate() {
                     />
                   </Label>
                 </div>
-                <div className="h-full flex flex-col gap-y-4 p-2 border-t">
+                <div className="h-4/10 flex flex-col  gap-y-4 p-2 border-t">
                   <h1 className="text-md font-semibold  text-[var(--text-primary)] pl-4 py-4 ">
                     Detection Settings
                   </h1>
 
-                  <Label className=" flex flex-col  w-full justify-center text-md font-medium text-[var(--text-primary)] text-left pl-1">
+                  <Label className=" flex flex-col  w-full text-md font-medium text-[var(--text-primary)] text-left pl-1">
                     Detection on
                     <Switch
                       checked={detectionSettings.runDetection}
@@ -70,7 +70,7 @@ export default function VM_SessionTemplate() {
                       }}
                     />
                   </Label>
-                  <Label className=" flex flex-col  w-full justify-center text-md font-medium text-[var(--text-primary)] text-left pl-1">
+                  <Label className=" flex flex-col  w-full  text-md font-medium text-[var(--text-primary)] text-left pl-1">
                     Detection Interval
                     <Input
                       data-testid="course-Page-input"
@@ -82,6 +82,38 @@ export default function VM_SessionTemplate() {
                           DetectionInterval: Number(e.target.value),
                         }));
                       }}
+                      min={1}
+                      max={100}
+                      type="number"
+                      placeholder="0"
+                      className="h-8 w-40 rounded-md border border-[var(--border)] bg-transparent px-2 text-sm text-[var(--text-primary)] focus:outline-none focus:ring-1 focus:ring-[var(--ring)]"
+                    />
+                  </Label>
+                </div>
+                <div className="h-4/10 flex flex-col  gap-y-4 p-2 border-t">
+                  <h1 className="text-md font-semibold  text-[var(--text-primary)] pl-4 py-4 ">
+                    Inference Settings
+                  </h1>
+
+                  <Label className=" flex flex-col  w-full text-md font-medium text-[var(--text-primary)] text-left pl-1">
+                    Run Inference
+                    <Switch
+                      checked={detectionSettings.runDetection}
+                      onCheckedChange={(e) => {
+                        setDetectionSettings((settings) => ({
+                          ...settings,
+                          runDetection: e,
+                        }));
+                      }}
+                    />
+                  </Label>
+                  <Label className=" flex flex-col  w-full  text-md font-medium text-[var(--text-primary)] text-left pl-1">
+                    Inference Interval
+                    <Input
+                      data-testid="course-Page-input"
+                      id="course-page"
+                      value={detectionSettings.DetectionInterval}
+                      onChange={(e) => {}}
                       min={1}
                       max={100}
                       type="number"
