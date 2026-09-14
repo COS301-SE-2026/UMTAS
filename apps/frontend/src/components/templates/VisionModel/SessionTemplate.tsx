ebb8e872eddd12281ed4c14370f64791649d8f0c
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-14 11:05:24 +0200

    feat(VisionModel): settings is hard to make look good

--- apps/frontend/src/components/templates/VisionModel/SessionTemplate.tsx
+++ apps/frontend/src/components/templates/VisionModel/SessionTemplate.tsx
@@ -16,55 +16,31 @@ export default function VM_SessionTemplate() {
         <h1 className="text-lg font-semibold text-[var(--text-primary)] pl-4 pt-4">
           Session Camera
         </h1>
-        <div className="grid gridgrid-cols-1 w-full h-full ">
-          <div className="flex justify-center items-center w-full min-h-[40vh]  ">
+        <div className="flex flex-col justify-around w-full h-full just">
+          <div className="flex justify-center items-center w-full min-h-[50vh] max-h-[50vh]  md:min-h-[60vh] md:max-h-[60vh]  ">
             <CameraCanvas isCameraActive={cameraOn} />
           </div>
-          <div className="h-full  items-center flex flex-col  w-full ">
+          <div className="  items-center flex flex-col  w-full  h-[10vh] min-h-[15vh] max-h-[15vh]">
             {/* ------------ Settings hier asseblief --------------- */}
             <div className="w-full h-full overflow-auto  rounded-t-none  border-t  border-[var(--border)]  bg-[var(--bg-surface)] shadow-sm flex flex-col">
-              <h1 className="text-md font-semibold text-[var(--text-primary)] pl-4 py-4">
-                Session Settings
-              </h1>
-              <div className="grid grid-cols-2 md:grid-cols-4 min-h-[10vh] justify-center w-full h-full text-center p-2">
-                <div className="h-full border grid grid-cols-1 ">
-                  <h1 className="text-md font-semibold text-[var(--text-primary)] pl-4 py-4">
-                    Session Settings
+              <div className="grid grid-cols-1 divide-y gap-5    md:grid-cols-3 min-h-[10vh] justify-center w-full h-full text-center p-2">
+                <div className="h-full flex flex-col gap-y-5 p-2">
+                  <h1 className="text-md font-semibold  text-[var(--text-primary)] pl-4 py-4 ">
+                    Camera Settings
                   </h1>
-                  <Label className=" flex flex-col text-md font-medium text-[var(--text-primary)] text-left pl-1">
+                  <Label className=" flex flex-row w-full justify-center text-md font-medium text-[var(--text-primary)] text-left pl-1">
                     Camera On
-                    <Switch className="border border-[var(--border)]" />
-                  </Label>
-                </div>
-                <div className="h-full border grid grid-cols-1 ">
-                  <h1 className="text-md font-semibold text-[var(--text-primary)] pl-4 py-4">
-                    Session Settings
-                  </h1>
-                  <Label className=" flex flex-col text-md font-medium text-[var(--text-primary)] text-left pl-1">
-                    Camera On
-                    <Switch className="border border-[var(--border)]" />
-                  </Label>
-                </div>
-                <div className="h-full border grid grid-cols-1 ">
-                  <h1 className="text-md font-semibold text-[var(--text-primary)] pl-4 py-4">
-                    Session Settings
-                  </h1>
-                  <Label className=" flex flex-col text-md font-medium text-[var(--text-primary)] text-left pl-1">
-                    Camera On
-                    <Switch className="border border-[var(--border)]" />
-                  </Label>
-                </div>
-                <div className="h-full border grid grid-cols-1 ">
-                  <h1 className="text-md font-semibold text-[var(--text-primary)] pl-4 py-4">
-                    Session Settings
-                  </h1>
-                  <Label className=" flex flex-col text-md font-medium text-[var(--text-primary)] text-left pl-1">
-                    Camera On
-                    <Switch className="border border-[var(--border)]" />
+                    <Switch
+                      checked={cameraOn}
+                      onCheckedChange={(e) => {
+                        setCameraOn(e);
+                      }}
+                    />
                   </Label>
                 </div>
               </div>
             </div>
+            {/* ------------ Settings hier asseblief --------------- */}
           </div>
         </div>
       </div>
