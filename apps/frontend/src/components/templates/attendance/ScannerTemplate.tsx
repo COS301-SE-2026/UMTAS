6e2f1047574a1b12b82b56479c3ef2ec6a1e9a8e
jcoet-gh <johancoet9@gmail.com>
2026-09-15 11:25:36 +0200

    feat: added attendance counter component to scanner

---
+++ apps/frontend/src/components/templates/attendance/ScannerTemplate.tsx
@@ -0,0 +1,35 @@
+import { Badge } from "@/components/atoms/baseShadcn/badge";
+import { Progress } from "@/components/atoms/baseShadcn/progress";
+import AttendanceCounter from "@/components/organisms/attendance/AttendanceCounter";
+
+export default function ScannerTemplate() {
+  //   function BarcodeScanner() {
+  //     return <></>;
+  //   }
+
+  //   function LastScannedStudent() {}
+  return (
+    <>
+      <div className="flex items-center flex-col">
+        <div className="mb-8">
+          <h1 className="text-2xl font-semibold text-[var(--text-primary)]">
+            Attendance Scanner
+          </h1>
+          <p className="text-sm font-normal text-[var(--text-secondary)]">
+            Scan Student Cards to Record Attendance
+          </p>
+        </div>
+        <div className="mb-2">
+          <Badge variant="secondary">Ready to Scan</Badge>
+        </div>
+
+        <div className="border-2 rounded-xl aspect-video w-[50%] mb-4">
+          {/* Barcode scanner goes here */}
+        </div>
+        <div className="w-[50%]">
+          <AttendanceCounter numberAttended={20} numberExpected={50} />
+        </div>
+      </div>
+    </>
+  );
+}
