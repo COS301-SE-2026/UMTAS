25e5e3bb27e9b8c108fa1eff1df44c6f0793247e
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-17 09:56:00 +0200

    feat(VisionModel): added a struct for the buffers compute shader needs

--- apps/frontend/wasm-engine/src/image_upscaler.rs
+++ apps/frontend/wasm-engine/src/image_upscaler.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use wgpu::util::DeviceExt;
 
 #[wasm_bindgen]
 pub fn slice_image_data_gpu(
@@ -9,4 +10,16 @@ pub fn slice_image_data_gpu(
     let result_arr = js_sys::Array::new();
 
     return Ok(result_arr);
+}
+
+// Buffer setup
+pub struct Buffers {
+    // where we put raw pixel data
+    pub input_buffer: wgpu::Buffer,
+    // where we put the slice
+    pub output_buffer: wgpu::Buffer,
+    // A map read buffer we use to get data from gpu to wasm
+    pub staging_buffer: wgpu::Buffer,
+    // parms needed to make width height
+    pub params_buffer: wgpu::Buffer,
 }
