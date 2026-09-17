369dc834c0ceed27f323da4399f2946ce26f9a49
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-17 09:46:24 +0200

    feat(VisionModel): setup signature properly

--- apps/frontend/wasm-engine/src/image_upscaler.rs
+++ apps/frontend/wasm-engine/src/image_upscaler.rs
@@ -1,7 +1,12 @@
 use wasm_bindgen::prelude::*;
 
-
 #[wasm_bindgen]
-pub fn slice_image_data_gpu(pixel_data: &[u8], width: usize, height: usize) {
-    
+pub fn slice_image_data_gpu(
+    pixel_data: &[u8],
+    width: usize,
+    height: usize,
+) -> Result<js_sys::Array, JsValue> {
+    let result_arr = js_sys::Array::new();
+
+    return Ok(result_arr);
 }
