1bc5e4a6ba2022251315f8e9f1d891466f7e4c1a
jcoet-gh <johancoet9@gmail.com>
2026-09-17 18:40:46 +0200

    fix: fixed module import error

--- apps/backend/src/Building/building.module.ts
+++ apps/backend/src/Building/building.module.ts
@@ -3,9 +3,10 @@ import { BuildingService } from './building.service';
 import { BuildingController } from './building.controller';
 import { UniversityModule } from 'src/University/university.module';
 import { VenueModule } from 'src/Venue/venue.module';
+import { EventModule } from 'src/Events/event.module';
 
 @Module({
-  imports: [UniversityModule, forwardRef(() => VenueModule)],
+  imports: [UniversityModule, forwardRef(() => VenueModule), EventModule],
   controllers: [BuildingController],
   exports: [BuildingService],
   providers: [BuildingService],
