d7a5d8a5cb0e07526628d31889793094be9a95c4
jcoet-gh <johancoet9@gmail.com>
2026-09-18 14:11:37 +0200

    fix: fixed incorrect query parameter for building request builder

--- apps/frontend/utilities/building/buildingRequestBuilder.ts
+++ apps/frontend/utilities/building/buildingRequestBuilder.ts
@@ -85,7 +85,7 @@ export class getAllBuildingsHeatmapBuilder extends RequestBuilder<
   undefined,
   undefined,
   getAllBuildingsHeatmapRes,
-  getAllBuildingsQuery
+  getAllBuildingsHeatmapQuery
 > {
   constructor() {
     super();
