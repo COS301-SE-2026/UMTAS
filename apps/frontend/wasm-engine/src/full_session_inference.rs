11b204136c26f54b89a440dd710734f9bf7d36e6
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-21 15:36:59 +0200

    feat(VisionModel): adding the people into the frame analysis

--- apps/frontend/wasm-engine/src/full_session_inference.rs
+++ apps/frontend/wasm-engine/src/full_session_inference.rs
@@ -1,6 +1,7 @@
-use image::Frames;
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
+use crate::pose_inference::Keypoint;
 use crate::pose_parse::FrameStore;
 
 #[wasm_bindgen]
@@ -13,16 +14,50 @@ pub fn analyse_session(frames: JsValue) -> Result<String, JsValue> {
 
 // needs to analyse per person so accumulate for an ID and then check distance between each and so on
 pub fn analyse_questions(frames: Vec<FrameStore>) {
+    // must remain sorted
+    let mut all_session_people: HashMap<usize, SessionPerson> = HashMap::new();
 
-    
-}
-pub fn analyse_questions_id(frames: Vec<FrameStore>) {
+    for frame in frames {
+        for person in frame.people {
+            let id = &person.assigned_id;
 
-    
+            if let Some(stored_person) = all_session_people.get_mut(id) {
+                stored_person
+                    .all_center_mass
+                    .push(person.pose_data.center_mass);
+
+                // question logic
+
+                
+            } else {
+                all_session_people.insert(
+                    person.assigned_id,
+                    SessionPerson {
+                        count_hand_up: if person.hand_up { 1 } else { 0 },
+                        last_frame_hand_up: if person.hand_up {
+                            Some(frame.frame_number)
+                        } else {
+                            None
+                        },
+                        assigned_id: person.assigned_id,
+                        all_center_mass: [person.pose_data.center_mass].to_vec(),
+                    },
+                );
+            }
+        }
+    }
 }
 
 pub struct session_analysis {
     questions_asked: usize,
     // a measure of everyones center and the average movement of that point
     mean_movement: usize,
+}
+
+#[derive(Clone)]
+pub struct SessionPerson {
+    count_hand_up: usize,
+    last_frame_hand_up: Option<usize>,
+    assigned_id: usize,
+    all_center_mass: Vec<Keypoint>,
 }
