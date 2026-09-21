d38c9f3b37083f63543d1df09efc89c95bbcaa8d
Wilmar-Smit <u24584216@tuks.co.za>
2026-09-21 16:17:19 +0200

    feat(VisionModel): function is diff

--- apps/frontend/wasm-engine/src/full_session_inference.rs
+++ apps/frontend/wasm-engine/src/full_session_inference.rs
@@ -16,6 +16,8 @@ pub fn analyse_session(frames: JsValue) -> Result<String, JsValue> {
 pub fn analyse_questions(frames: Vec<FrameStore>) {
     // must remain sorted
     let mut all_session_people: HashMap<usize, SessionPerson> = HashMap::new();
+    const DISTANCE_BETWEEN_START_END: usize = 10;
+    const DISTANCE_BETWEEN_END_NEW: usize = 10;
 
     for frame in frames {
         for person in frame.people {
@@ -28,17 +30,61 @@ pub fn analyse_questions(frames: Vec<FrameStore>) {
 
                 // question logic
 
-                
+                if person.hand_up {
+                    if let Some(first_frame_up) = stored_person.first_frame_hand_up
+                        && let Some(last_frame_up) = stored_person.last_frame_hand_up
+                    {
+                        // Hand is up and there has been a hand up before
+
+                        let distance_between_first_last = last_frame_up - first_frame_up;
+                        if let Some(hand_went_down) = stored_person.frame_hand_down {
+                            // the hand has been up before and has gone down
+                            let distance_between_down_new = frame.frame_number - hand_went_down;
+
+                            if distance_between_down_new > DISTANCE_BETWEEN_END_NEW {
+                                // hand has been up down long enough to begin again
+                                stored_person.first_frame_hand_up = Some(frame.frame_number);
+                                stored_person.last_frame_hand_up = Some(frame.frame_number);
+                                stored_person.frame_hand_down = None;
+                            } else {
+                                // hand has not gone down since last time update it to current frame
+                                stored_person.frame_hand_down = Some(frame.frame_number);
+                            }
+                        } else {
+                            // the hand has been up before but has not gone down
+                            if distance_between_first_last > DISTANCE_BETWEEN_START_END {
+                                // hand has been up long enough to count the hand up
+                                stored_person.count_hand_up += 1;
+                                stored_person.first_frame_hand_up = None;
+                                stored_person.last_frame_hand_up = None;
+                                stored_person.frame_hand_down = Some(frame.frame_number);
+                            } else {
+                                // hand has not been up long enough for hand to go up
+                                stored_person.last_frame_hand_up = Some(frame.frame_number);
+                            }
+                        }
+                    } else {
+                        // hand is up there was not a hand before
+                        stored_person.first_frame_hand_up = Some(frame.frame_number);
+                        stored_person.last_frame_hand_up = Some(frame.frame_number);
+                    }
+                }
             } else {
                 all_session_people.insert(
                     person.assigned_id,
                     SessionPerson {
                         count_hand_up: if person.hand_up { 1 } else { 0 },
+                        first_frame_hand_up: if person.hand_up {
+                            Some(frame.frame_number)
+                        } else {
+                            None
+                        },
                         last_frame_hand_up: if person.hand_up {
                             Some(frame.frame_number)
                         } else {
                             None
                         },
+                        frame_hand_down: None,
                         assigned_id: person.assigned_id,
                         all_center_mass: [person.pose_data.center_mass].to_vec(),
                     },
@@ -57,7 +103,9 @@ pub struct session_analysis {
 #[derive(Clone)]
 pub struct SessionPerson {
     count_hand_up: usize,
+    first_frame_hand_up: Option<usize>,
     last_frame_hand_up: Option<usize>,
+    frame_hand_down: Option<usize>,
     assigned_id: usize,
     all_center_mass: Vec<Keypoint>,
 }
